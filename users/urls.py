3f8f21521d94392f7d2b7b4f8ad757cbc01021f9
senyehor <senyehor@gmail.com>
2022-06-20 18:17:05 +0300

    created login functionality

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 
 from users import views
 
+overridden_account_patterns = [
+    path('login', views.LoginView.as_view(template_name='registration/login.html')),
+]
+
 urlpatterns = [
-    path('add-user', views.debug)
+    include('django.contrib.auth.urls'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
-from users.models import User
+from datetime import timedelta
+
+from django import forms
+from django.contrib.auth.views import LoginView as LogView
+
+from users.forms import LoginForm
 
 
-def debug(request):
-    tmp = User.objects.create()
-    return
+class LoginView(LogView):
+    form_class = LoginForm
+    remember_me = forms.BooleanField(required=False, label="Запам'ятати мене на два тижні")
+
+    def form_valid(self, form):
+        if form.data.get('remember_me'):
+            self.request.session.set_expiry(timedelta(weeks=2))
+        return super().form_valid(form)
