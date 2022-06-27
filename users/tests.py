d90d4905ac032853746237b6a1ca7dc63523aaa5
Yehor Sencha <senyehor@gmail.com>
2022-06-27 15:27:19 +0300

    removed using gettext_lazy and finished user registration request testing

--- users/tests.py
+++ users/tests.py
@@ -3,14 +3,14 @@ from typing import TypedDict
 
 import factory
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist, ValidationError
 from django.http import HttpResponse
-from django.test import TestCase, Client
+from django.test import Client, TestCase
 from django.urls import reverse_lazy
-from django.utils.translation import gettext_lazy as _
 from factory import django
 
 from institutions.tests import InstitutionFactory
+from users.models import UserRegistrationRequest
 
 User = get_user_model()
 
@@ -26,19 +26,19 @@ class UserTest(TestCase):
         raw_password = self.some_password
         name = self.valid_full_name
         user = User.objects.create(email=email, password=raw_password, full_name=name)
-        self.assertEqual(email, user.email, _('user email was set incorrectly'))
-        self.assertTrue(user.check_password(raw_password), _('user password was set incorrectly'))
-        self.assertEqual(name, user.full_name, _('user full name was set incorrectly'))
-        self.assertEqual(email, user.get_username(), _('email is not used as username'))
+        self.assertEqual(email, user.email, 'user email was set incorrectly')
+        self.assertTrue(user.check_password(raw_password), 'user password was set incorrectly')
+        self.assertEqual(name, user.full_name, 'user full name was set incorrectly')
+        self.assertEqual(email, user.get_username(), 'email is not used as username')
 
     def test_creating_with_incorrect_fields_sets(self):
-        with self.assertRaises(TypeError, msg=_('User was created with wrong fields set')):
+        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
             User.objects.create()
-        with self.assertRaises(TypeError, msg=_('User was created with wrong fields set')):
+        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
             User.objects.create(full_name=self.valid_full_name)
-        with self.assertRaises(TypeError, msg=_('User was created with wrong fields set')):
+        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
             User.objects.create(email=self.valid_email)
-        with self.assertRaises(TypeError, msg=_('User was created with wrong fields set')):
+        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
             User.objects.create(full_name=self.valid_full_name, email=self.valid_email)
 
     def test_full_name_check(self):
@@ -53,7 +53,7 @@ class UserTest(TestCase):
                     user.delete()
                 except ValidationError:
                     if not should_throw:
-                        assert False, _('validating full name throws exception for a valid name')
+                        assert False, 'validating full name throws exception for a valid name'
 
         correct_names_list = [
             """П'ятницький Хотибор Денисович""",
@@ -124,7 +124,9 @@ class UserRegistrationRequestFormsetTest(TestCase):
         # using .build() to not save user
         self.user: User = UserFactory.build(password=self.raw_password)
         self.institution = InstitutionFactory()
-        # doto separate
+
+    @staticmethod
+    def __get_form_data() -> dict[str, str]:
         data = TypedDict(
             'data',
             {
@@ -160,20 +162,19 @@ class UserRegistrationRequestFormsetTest(TestCase):
         sample_data['registration_requests-MIN_NUM_FORMS'] = '1'
         sample_data['registration_requests-MAX_NUM_FORMS'] = '1'
 
-        def get_form_data() -> data:
-            nonlocal sample_data
-            return sample_data.copy()
-
-        self.get_form_data = get_form_data
+        return sample_data.copy()
 
     def test_with_correct_data_set(self):
-        data = self.get_form_data()
+        # HUGE WARNING: when debugging something VERY strange is
+        # happening, (if I have a breakpoint outside of test)
+        # UserRegistrationRequestForm will fail validation with no apparent reason
+        # django request ticket # todo
+        data = self.__get_form_data()
         data['full_name'] = self.user.full_name
         data['email'] = self.user.email
         data['password1'] = self.raw_password
         data['password2'] = self.raw_password
-        data['registration_requests-0-institution'] = str(self.institution.institution_id)
-        resp = self.client.get(reverse_lazy('register'))
+        data['registration_requests-0-institution'] = str(self.institution.pk)
         resp: HttpResponse = self.client.post(
             reverse_lazy('register'),
             {
@@ -181,4 +182,30 @@ class UserRegistrationRequestFormsetTest(TestCase):
             },
             follow=True
         )
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.status_code,
+            200,
+            'form validation failed with correct data'
+        )
+        try:
+            user = User.objects.get(
+                full_name=self.user.full_name,
+                email=self.user.email
+            )
+        except ObjectDoesNotExist:
+            assert False, 'user is not created'
+        except MultipleObjectsReturned:
+            assert False, 'multiple users are created'
+        try:
+            user_registration_request = UserRegistrationRequest.objects.get(
+                user=user
+            )
+        except ObjectDoesNotExist:
+            assert False, 'user registration request is not created'
+        except MultipleObjectsReturned:
+            assert False, 'multiple user registration requests are created'
+        self.assertEqual(
+            user_registration_request.institution.pk,
+            self.institution.pk,
+            'incorrect institution is saved for registration request'
+        )
