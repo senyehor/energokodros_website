dde1d46fbb130988430bcee7c8abedd284160c1f
Yehor Sencha <senyehor@gmail.com>
2022-06-28 12:53:09 +0300

    created model to separate user registration requests and actual system users + some logic included in registration process

--- users/logic.py
+++ users/logic.py
@@ -1,9 +1,20 @@
+from dataclasses import dataclass
 from datetime import timedelta
+from hashlib import sha256
+from typing import Callable
 
 from django.forms import BaseInlineFormSet
 from django.http import HttpRequest
 
-from users.forms import UserRegistrationRequestFormset
+
+@dataclass
+class UserRegistrationCode:
+    __algorithm: Callable[[bytes], str] = sha256
+    LENGTH = 64  #
+
+    @classmethod
+    def generate(cls, email: str) -> str:
+        return cls.__algorithm(email.encode()).hexdigest()
 
 
 def remember_user_for_two_week(request: HttpRequest):
@@ -19,3 +30,7 @@ def _get_user_registration_formset(formset: BaseInlineFormSet) \
     # formsets are supposed to work with bulk data, but we just need it to
     # join NewUser + UserRegistrationRequestForm in quantity 1, so we fearlessly take [0]
     return formset.save(commit=False)[0]
+
+
+def _generate_code_for_email(email: str) -> str:
+    return UserRegistrationCode.generate(email)
