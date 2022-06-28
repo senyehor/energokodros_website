e2b3c2756e9d3ee7452f7634dd95979273bb254a
Yehor Sencha <senyehor@gmail.com>
2022-06-28 11:46:16 +0300

    added optional / to home page and reordered installed_apps

--- energokodros/urls.py
+++ energokodros/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path, re_path
 import users.views
 
 urlpatterns = [
-    re_path(r'^$|home', users.views.index_view),
+    re_path(r'^$|home/?', users.views.index_view, name='home'),
     re_path(r'users/', include('users.urls')),
     path('administrator/', include('administrator.urls')),
 ]
