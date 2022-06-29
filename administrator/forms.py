9c354e95af854b094dc22ebf2cf6fbb27c896e01
Yehor Sencha <senyehor@gmail.com>
2022-06-29 12:02:03 +0300

    added applied_at for user registration data and renamed UserRegistrationRequest to UserRoleApplication (and everything that used it correspondingly), as it is more correct

--- administrator/forms.py
+++ administrator/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from institutions.models import AccessLevel
-from users.models import UserRegistrationRequest
+from users.models import UserRoleApplication
 
 
 class UserRegistrationRequestsDecisionForm(forms.Form):
-    users_registration_requests = forms.ModelChoiceField(
-        queryset=UserRegistrationRequest.objects.all(),
+    users_role_applications = forms.ModelChoiceField(
+        queryset=UserRoleApplication.objects.all(),
         label=_('Запити користувачів на реєстрацію')
     )
     access_level = forms.ModelChoiceField(
