1a64b466e706f329e4d14d3108b09fccfa44dd1f
Yehor Sencha <senyehor@gmail.com>
2022-06-29 15:16:14 +0300

    changed CreateUserRegistrationRequest form accordingly to user registration process changes and updated template

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView as LogView
+from django.forms import BaseInlineFormSet
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
@@ -8,9 +9,10 @@ from django.views.generic import CreateView
 from users.forms import (
     LoginForm,
     NewUserForm,
-    UserRoleApplicationAndRegistrationFormset
+    RegistrationFormset
 )
-from users.logic import _get_user_registration_formset, remember_user_for_two_week
+from users.logic import RegistrationController, remember_user_for_two_week
+from users.models import User
 
 
 class LoginView(LogView):
@@ -25,45 +27,47 @@ class LoginView(LogView):
 class CreateUserRegistrationRequest(CreateView):
     form_class = NewUserForm
     template_name = 'registration/register.html'
+    controller = RegistrationController
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['user_role_application_and_registration_formset'] = \
-            UserRoleApplicationAndRegistrationFormset()
+        ctx['registration_formset'] = RegistrationFormset()
         return ctx
 
     def post(self, request, *args, **kwargs):
         self.object = None  # noqa
         form = self.get_form(self.get_form_class())
-        user_role_application_and_registration_formset = \
-            UserRoleApplicationAndRegistrationFormset(self.request.POST or None)
-        if form.is_valid() and user_role_application_and_registration_formset.is_valid():
-            return self.form_valid(form, user_role_application_and_registration_formset)
-        return self.form_invalid(form, user_role_application_and_registration_formset)
+        registration_formset = RegistrationFormset(self.request.POST or None)
+        if form.is_valid() and registration_formset.is_valid():
+            return self.form_valid(form, registration_formset)
+        return self.form_invalid(form, registration_formset)
 
-    def form_valid(  # noqa pylint: disable=W0221
-            self, form: NewUserForm,
-            user_role_application_and_registration_formset:
-            UserRoleApplicationAndRegistrationFormset):
+    def form_valid(self, form: NewUserForm, registration_formset: RegistrationFormset):  # noqa pylint: disable=W0221
         self.object = form.save(commit=False)  # noqa
         self.object.save()
-        form = _get_user_registration_formset(user_role_application_and_registration_formset)
+        form = self.__get_user_from_registration_formset(registration_formset)
         form.user = self.object
         form.save()
-        return redirect(reverse_lazy('successfully_created_registration_request'))
+        return redirect(reverse_lazy('home'))
 
     def form_invalid(  # noqa pylint: pylint: disable=W0221
             self, form: NewUserForm,
-            user_role_application_and_registration_formset:
-            UserRoleApplicationAndRegistrationFormset):
+            registration_formset:
+            RegistrationFormset):
         return self.render_to_response(
             self.get_context_data(
                 form=form,
-                user_role_application_and_registration_formset=
-                user_role_application_and_registration_formset,
+                registration_formset=
+                registration_formset,
             ),
             status=400
         )
+
+    @staticmethod
+    def __get_user_from_registration_formset(registration_formset: BaseInlineFormSet) -> User:
+        # here we take [0], as formset is supposed to work with bulk data,
+        # but is used here to join user creating from and user role application form
+        return registration_formset.save(commit=False)[0]
 
 
 @login_required
