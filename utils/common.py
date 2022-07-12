2d5aeac8d730b99708c874c1d92cdf256ac6b04f
Yehor Sencha <senyehor@gmail.com>
2022-07-12 13:47:24 +0300

    changed __send_email to send_html_email

--- utils/common.py
+++ utils/common.py
@@ -1,27 +1,29 @@
 from smtplib import SMTPException
 
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 
 
 def try_send_email_add_warning_if_failed(
         request: HttpRequest, email: str, subject: str, message: str):
-    if not __send_email(email, subject, message):
+    if send_html_email(email, subject, message):
         messages.warning(request, _(f'Не вдалося надіслати повідомлення на пошту {email}'))
 
 
-def __send_email(email: str, subject: str, message: str) -> bool:
+def send_html_email(email: str, subject: str, html: str) -> bool:
+    msg = EmailMultiAlternatives(
+        subject=subject,
+        body=html,
+        # django requires from_email to be explicitly set to None
+        # to use settings.DEFAULT_FROM_EMAIL
+        from_email=None,
+        to=[email]
+    )
+    msg.content_subtype = 'html'
     try:
-        send_mail(
-            subject,
-            message,
-            # django requires from_email to be explicitly set to None
-            # to use settings.DEFAULT_FROM_EMAIL
-            from_email=None,
-            recipient_list=[email]
-        )
+        msg.send()
     except SMTPException:
         return False
     return True
