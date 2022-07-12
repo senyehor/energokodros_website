b4fd9ab9d8b0026ea11a7375f1dce248dd77f4cd
Yehor Sencha <senyehor@gmail.com>
2022-07-12 13:24:37 +0300

    created UserRoleApplicationDecision view

--- administrator/views.py
+++ administrator/views.py
@@ -1,53 +1,77 @@
-from django.http import HttpRequest, HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import FormView, ListView
 
 from administrator.decorators import admin_rights_required
-from administrator.forms import UserRegistrationRequestsDecisionForm
-from administrator.logic import _get_message_for_role_application
+from administrator.forms import UserRoleApplicationRequestsDecisionForm
+from administrator.logic import (
+    _check_user_has_no_roles,
+    _get_applications_from_users_who_confirmed_email_ordered, UserRoleApplicationReviewController,
+)
+from users.models import UserRoleApplication
 
 
 @admin_rights_required
 def admin_page(request: HttpRequest):
     return render(
         request,
-        'administrator.html',
+        'administrator/administrator.html',
         {
-            'pending_registration_requests_form': UserRegistrationRequestsDecisionForm()
+            'pending_registration_requests_form': UserRoleApplicationRequestsDecisionForm()
         }
     )
 
 
-class RegistrationRequestReview(View):
-    http_method_names = ['post']
-
-    def post(self, request: HttpRequest):  # noqa
-        pass
-        # f = request.POST
-        # try:
-        #     registration_request = UserRegistrationRequest.objects.get(
-        #         id=f['user_registration_request']
-        #     )
-        #     if decision.lower() == 'схвалити':
-        #         UserRole(
-        #             position=f['position'],
-        #             access_level_id=f['access_level'],
-        #             institution=registration_request.institution,
-        #             user=registration_request.user
-        #         ).save()
-        #     elif decision.lower() == 'відмовити':
-        #         registration_request.user.delete()
-        #     else:
-        #         return HttpResponse(status=400)
-        #     registration_request.delete()
-        #     return redirect('admin_page')
-        # except KeyError:
-        #     return HttpResponse(status=400)
+@admin_rights_required
+class UserRoleApplicationsList(ListView):
+    # pagination without ordering might be inconsistent
+    queryset = _get_applications_from_users_who_confirmed_email_ordered()
+    context_object_name = 'applications'
+    paginate_by = 10
+    template_name = 'administrator/user_role_applications.html'
 
 
 @admin_rights_required
-def get_message_for_registration_request(request: HttpRequest):
-    if request_id := request.POST.get('request_id'):  # noqa
-        return JsonResponse(
-            {'message': _get_message_for_role_application(request_id)},
+class UserRoleApplicationDecision(FormView):
+    http_method_names = ['get', 'post']  # removing PUT method that is in FormView by default
+    template_name = 'administrator/user_role_application.html'
+    form_class = UserRoleApplicationRequestsDecisionForm
+    success_url = reverse_lazy('users_roles_applications')
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['user_has_no_roles'] = _check_user_has_no_roles(self.__get_application_request().user)
+        return ctx
+
+    def get_form(self, form_class=None):
+        return UserRoleApplicationReviewController.get_application_decision_form_for_application(
+            self.__get_application_request()
         )
-    return HttpResponse(status=405)
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        form = self.form_class(request.POST or None)
+        self.controller = UserRoleApplicationReviewController(  # noqa
+            form,
+            self.__get_application_request(),
+            self.request,
+        )
+        return self.form_valid(form) if form.is_valid() else self.form_invalid(form)
+
+    def form_invalid(self, form: UserRoleApplicationRequestsDecisionForm):
+        if not (is_approved := self.application_approved):
+            # case when admin decided to decline application,
+            # so we do not care about any form validation
+            self.controller.process_depending_on_decision(is_approved)
+        return super().form_invalid(form)
+
+    def form_valid(self, form: UserRoleApplicationRequestsDecisionForm):
+        self.controller.process_depending_on_decision(self.application_approved)
+        return redirect(reverse('users_roles_applications'))
+
+    def __get_application_request(self):
+        return get_object_or_404(UserRoleApplication, pk=self.kwargs['pk'])
+
+    @property
+    def application_approved(self):
+        return self.request.POST['decision'] == 'accept'
