86536fb68ba58544427152a34599c0a3e6b9f44f
Yehor Sencha <senyehor@gmail.com>
2022-07-13 15:43:15 +0300

    refactored tests to be packages

--- users/tests.py
+++ users/tests.py
@@ -1,119 +1,19 @@
-import random
 from typing import TypedDict
 
-import factory
 from django.contrib.auth import get_user_model
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist, ValidationError
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.http import HttpResponse
 from django.test import Client, RequestFactory, TestCase
 from django.urls import reverse, reverse_lazy
-from factory import django
 
 from institutions.models import Institution
 from institutions.tests import InstitutionFactory
 from users.logic import EmailConfirmationController
 from users.models import UserRoleApplication
+from users.tests.factories import UserFactory
 from utils.crypto import hide_int
 
 User = get_user_model()
-
-
-class UserTest(TestCase):
-    def setUp(self):
-        self.valid_email = 'valid@email.com'
-        self.some_password = 'some_password'
-        self.valid_full_name = "Тестове Валідне Ім'я"
-
-    def test_creating_user(self):
-        email = self.valid_email
-        raw_password = self.some_password
-        name = self.valid_full_name
-        user = User.objects.create(email=email, password=raw_password, full_name=name)
-        self.assertEqual(email, user.email, 'user email was set incorrectly')
-        self.assertTrue(user.check_password(raw_password), 'user password was set incorrectly')
-        self.assertEqual(name, user.full_name, 'user full name was set incorrectly')
-        self.assertEqual(email, user.get_username(), 'email is not used as username')
-
-    def test_creating_with_incorrect_fields_sets(self):
-        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
-            User.objects.create()
-        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
-            User.objects.create(full_name=self.valid_full_name)
-        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
-            User.objects.create(email=self.valid_email)
-        with self.assertRaises(TypeError, msg='User was created with wrong fields set'):
-            User.objects.create(full_name=self.valid_full_name, email=self.valid_email)
-
-    def test_full_name_check(self):
-        def test_for_names_list(names: list[str], should_throw: bool):
-            for name in names:
-                try:
-                    User.objects.create(
-                        full_name=name,
-                        email=self.valid_email,
-                        password=self.some_password
-                    )
-                except ValidationError:
-                    if not should_throw:
-                        assert False, 'validating full name throws exception for a valid name'
-
-        correct_names_list = [
-            """П'ятницький Хотибор Денисович""",
-            """Никоненко Лаврентій-Арсен Полянович""",
-            """Слюсар Наслав Денисович""",
-        ]
-        incorrect_names_list = [
-            'Некоректне',
-            "Некоректне Ім'я",
-            'Incorrect Name Example',
-            'Некорректный Пример Имени',
-        ]
-        test_for_names_list(correct_names_list, should_throw=False)
-        test_for_names_list(incorrect_names_list, should_throw=True)
-
-    def test_creating_admin(self):
-        admin = User.objects.create(
-            full_name=self.valid_full_name,
-            email=self.valid_email,
-            password=self.some_password,
-            is_admin=True
-        )
-        self.assertTrue(admin.is_admin)
-
-
-class UserFactory(django.DjangoModelFactory):
-    password = factory.Sequence(lambda n: f'generated_password{n}')
-    email = factory.Sequence(lambda n: f'generated_email{n}@email.com')
-
-    @factory.lazy_attribute
-    def full_name(self):
-        first_names = [
-            'Яснолик-Гузь',
-            'Шарль',
-            'Златоус',
-            'Милована',
-        ]
-        second_names = [
-            'Оробко',
-            'Білоножко',
-            'Даценко',
-            'Гасенко',
-        ]
-        patronims = [
-            'Адріанович',
-            'Романович',
-            'Мстиславович',
-            "В'ячеславович"
-        ]
-        return " ".join([
-            random.choice(first_names),
-            random.choice(second_names),
-            random.choice(patronims)
-        ])
-
-    class Meta:
-        model = User
-
 
 _user_registration_data_dict = TypedDict(
     'data_dict',  # noqa
