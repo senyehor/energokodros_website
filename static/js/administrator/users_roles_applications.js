3c5c6c6a9bfb28fa23a8ee8b3326c11872de3ec7
Yehor Sencha <senyehor@gmail.com>
2022-07-13 14:40:20 +0300

    renaming and js separation

--- static/js/administrator/users_roles_applications.js
+++ static/js/administrator/users_roles_applications.js
@@ -1,10 +1,12 @@
 import {bg_light_on_hover} from "../common/common.js";
 
 $(document).ready(function () {
+        // set applications change bg on hover
         $('#applications > div').each(
             function () {
                 bg_light_on_hover($(this));
             }
         )
     }
-)
+);
+
