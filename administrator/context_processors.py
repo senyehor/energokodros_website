0e2bcb99fb538357860ec36fbb7215a547d549ba
Yehor Sencha <senyehor@gmail.com>
2022-07-13 14:35:23 +0300

    fixed user_roles_applications_to_review_count when no reviews

--- administrator/context_processors.py
+++ administrator/context_processors.py
@@ -1,10 +1,13 @@
-from administrator.logic import _get_applications_from_users_who_confirmed_email_ordered, _is_admin
+from administrator.logic.helper_functions import (
+    get_applications_from_users_who_confirmed_email_ordered,
+    is_admin,
+)
 
 
 def user_roles_applications_to_review_count(request):
-    if _is_admin(request):
+    if is_admin(request):
+        _ = get_applications_from_users_who_confirmed_email_ordered()
         return {
-            'users_roles_applications_count':
-                _get_applications_from_users_who_confirmed_email_ordered().count()
+            'users_roles_applications_count': _.count()
         }
-    return None
+    return {}
