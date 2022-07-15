ff9ab32028eccebe1ed6ec84e08f4f907cb7959e
Yehor Sencha <senyehor@gmail.com>
2022-07-15 10:40:54 +0300

    deleted unused function and added typehint

--- administrator/logic/helper_functions.py
+++ administrator/logic/helper_functions.py
@@ -1,14 +1,7 @@
 from django.db.models import QuerySet
 from django.http import HttpRequest
-from django.shortcuts import get_object_or_404
 
 from users.models import User, UserRoleApplication
-
-
-def get_message_for_role_application(application_id: int) -> UserRoleApplication:
-    # as we use SecureModelChoiceField we get request_id hashed,
-    # so converting it to int back
-    return get_object_or_404(UserRoleApplication, pk=application_id)
 
 
 def check_user_has_no_roles(user: User) -> bool:
@@ -19,5 +12,5 @@ def get_applications_from_users_who_confirmed_email_ordered() -> QuerySet:
     return UserRoleApplication.objects.all().filter(user__is_active=True).order_by('-pk')
 
 
-def is_admin(request: HttpRequest):
+def is_admin(request: HttpRequest) -> bool:
     return request.user.is_authenticated and request.user.is_admin  # noqa
