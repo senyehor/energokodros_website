beb0855a867fe21a881eadd7b4fbd06fb2be3ddd
Yehor Sencha <senyehor@gmail.com>
2022-07-18 13:28:17 +0300

    finished user role application form

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth import forms as auth_forms
-from django.forms import inlineformset_factory
+from django.utils.decorators import classonlymethod
 from django.utils.translation import gettext_lazy as _
 
 from institutions.models import Institution
 from users.models import User, UserRoleApplication
-from utils.forms import SecureModelChoiceField
+from utils.forms import CrispyFormsMixin, SecureModelChoiceField
 
 
 class LoginForm(auth_forms.AuthenticationForm):
@@ -26,10 +26,11 @@ class NewUserForm(auth_forms.UserCreationForm):
         fields = ('full_name', 'email')
 
 
-class UserRoleApplicationForm(forms.ModelForm):
+class UserRoleApplicationForm(forms.ModelForm, CrispyFormsMixin):
     institution = SecureModelChoiceField(
         label=_('Оберіть установу'),
         queryset=Institution.objects.all(),
+        required=True
     )
     message = forms.CharField(
         label=_('Залиште опціональне повідомлення'),
@@ -37,18 +38,29 @@ class UserRoleApplicationForm(forms.ModelForm):
         required=False,
         widget=forms.Textarea({'rows': 2})
     )
+    user = SecureModelChoiceField(
+        queryset=User.objects.all(),
+        required=True
+    )
 
     class Meta:
         model = UserRoleApplication
-        fields = ('institution', 'message')
+        fields = ('institution', 'message', 'user')
+        fields_to_hide = ('user',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_submit_button_at_the_end(
+            _('Відправити заявку на реєстрацію')
+        )
 
-RegistrationFormset = inlineformset_factory(
-    parent_model=User,
-    model=UserRoleApplication,
-    form=UserRoleApplicationForm,
-    extra=1,
-    can_delete=False,
-    min_num=1,
-    max_num=1
-)
+    @classonlymethod
+    def create_without_user_field(cls, *args, **kwargs):
+        """this method is intended to use for registration"""
+        obj = cls(*args, **kwargs)
+        obj.fields.pop('user', None)
+        return obj
+
+    def set_valid_user(self, user: User):
+        """be careful, no user validation is run here"""
+        self.instance.user = user
