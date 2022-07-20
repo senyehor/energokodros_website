532fb0f297d93cb7afb0a701f7f29ce5a394b43f
Yehor Sencha <senyehor@gmail.com>
2022-07-20 14:53:46 +0300

    small refactoring

--- static/js/common/page_items.js
+++ static/js/common/page_items.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
+    set_all_items_on_hover();
+})
+
+function set_all_items_on_hover() {
     $('#items > div').each(
         function () {
             bg_light_on_hover($(this));
         }
     )
-})
+}
+
 
 function bg_light_on_hover(elem) {
     let bg_class = 'bg-light';
