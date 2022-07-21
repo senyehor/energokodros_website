61ad20b6f59788f2f0b3dd3cb7bf9d3fb52f2c44
Yehor Sencha <senyehor@gmail.com>
2022-07-21 12:44:33 +0300

    renamed as function no longer orders by pk

--- administrator/context_processors.py
+++ administrator/context_processors.py
@@ -1,12 +1,12 @@
 from administrator.logic import (
-    get_applications_from_users_who_confirmed_email_ordered,
+    get_applications_from_users_who_confirmed_email,
     is_admin,
 )
 
 
 def user_roles_applications_to_review_count(request):
     if is_admin(request):
-        _ = get_applications_from_users_who_confirmed_email_ordered()
+        _ = get_applications_from_users_who_confirmed_email()
         return {
             'users_roles_applications_count': _.count()
         }
--- administrator/views/simple.py
+++ administrator/views/simple.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from administrator.decorators import admin_rights_required
-from administrator.logic import get_applications_from_users_who_confirmed_email_ordered
+from administrator.logic import get_applications_from_users_who_confirmed_email
 from energokodros.settings import DEFAULT_PAGINATE_BY
 from utils.filters import QuerySetFieldsIcontainsFilterPkOrderedMixin
 
@@ -18,7 +18,7 @@ def admin_page(request: HttpRequest):
 
 @admin_rights_required
 class UserRoleApplicationsListView(QuerySetFieldsIcontainsFilterPkOrderedMixin, ListView):
-    queryset = get_applications_from_users_who_confirmed_email_ordered()
+    queryset = get_applications_from_users_who_confirmed_email()
     filter_fields = ('user__full_name', 'user__email', 'institution__name')
     paginate_by = DEFAULT_PAGINATE_BY
     template_name = 'administrator/users_roles_applications.html'
