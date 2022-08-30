e4a09dfa41b75bf3074775a157b04c0de1f13ddb
Yehor Sencha <senyehor@gmail.com>
2022-08-30 09:36:42 +0300

    feat: created new institutions list and create views and corresponding urls

--- institutions/views/simple.py
+++ institutions/views/simple.py
@@ -1,11 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import FormView, ListView
 
 from energokodros.settings import DEFAULT_PAGINATE_BY
+from institutions.forms.institution_form import InstitutionForm
 from institutions.models import Facility
 
 
-class InstitutionsList(ListView):
+class InstitutionsListView(ListView):
     queryset = Facility.objects.get_institutions()
     filter_fields = ('name', 'description')
     paginate_by = DEFAULT_PAGINATE_BY
-    template_name = 'institutions'
+    template_name = 'institutions/institutions_list.html'
+
+
+class InstitutionCreateView(FormView):
+    form_class = InstitutionForm
+    template_name = 'institutions/new_institution.html'
