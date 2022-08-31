c279a75389d85570acd95351ad2ac91dd1598d85
Yehor Sencha <senyehor@gmail.com>
2022-08-31 12:33:47 +0300

    ref: created separate views package and distributed view from old views.py there

--- energokodros/urls.py
+++ energokodros/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path, re_path, register_converter
 
-import users.views
+from users.views import index_view
 from utils.crypto import IntHasher, StringHasher
 
 register_converter(IntHasher, 'hashed_int')
 register_converter(StringHasher, 'hashed_str')
 
 urlpatterns = [
-    re_path(r'^$|home', users.views.index_view, name='home'),
+    re_path(r'^$|home', index_view, name='home'),
     path('users/', include('users.urls')),
     path('administrator/', include('administrator.urls')),
 ]
