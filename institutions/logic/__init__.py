13474b41668d64a015a92441d18dc9cd67b7b7fc
Yehor Sencha <senyehor@gmail.com>
2022-09-26 11:54:38 +0300

    ref: moved facility formatting and ajax function to separate file

--- institutions/logic/__init__.py
+++ institutions/logic/__init__.py
@@ -1,1 +1,4 @@
-from .simple import compose_formatted_institution_facilities_choices, get_institution_by_hashed_id
+from .facility_formatting_and_ajax_related import (
+    compose_formatted_institution_facilities_choices,
+    get_institution_by_hashed_id,
+)
