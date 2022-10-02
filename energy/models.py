0a80a74a63e99864578a156b08b1a4876eba9be2
Yehor Sencha <senyehor@gmail.com>
2022-10-02 11:56:16 +0300

    fix: BoxSensorSet model field improved

--- energy/models.py
+++ energy/models.py
@@ -22,13 +22,38 @@ class Sensor(models.Model):
 
 
 class BoxSensorsSet(models.Model):
+    # todo think of better interface
     boxes_set_id = models.AutoField(primary_key=True)
-    box = models.ForeignKey(Box, models.CASCADE, null=False)
-    sensor = models.OneToOneField(Sensor, models.RESTRICT, null=False)
-    facility = models.ForeignKey(Facility, models.CASCADE, blank=True, null=True)
-    line_name = models.CharField(max_length=1000, blank=False, null=False)
-    date_set_in = models.DateTimeField(blank=True, null=True)
-    sensor_number = models.IntegerField()
+    box = models.ForeignKey(
+        Box,
+        models.CASCADE,
+        null=False,
+        blank=False
+    )
+    sensor = models.OneToOneField(
+        Sensor,
+        models.RESTRICT,
+        null=False,
+        related_name='+'
+    )
+    facility = models.ForeignKey(
+        Facility,
+        models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='box_sensors_set'
+    )
+    line_name = models.CharField(
+        max_length=1000,
+        blank=False,
+        null=False
+    )
+    date_set_in = models.DateTimeField(
+        auto_now_add=True,
+        null=False
+    )
+    # todo finish sensor_number
+    sensor_number = models.IntegerField(null=False)
 
     class Meta:
         db_table = 'boxes_sets'
