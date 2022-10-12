2de4843c036b65e6ed4a4560828c7c6d8b544b95
Yehor Sencha <senyehor@gmail.com>
2022-10-12 14:54:14 +0300

    fix: updated corresponding to new utils funcs

--- static/js/institutions/new_facility.js
+++ static/js/institutions/new_facility.js
@@ -5,28 +5,18 @@ $(document).ready(function () {
 })
 
 function update_parent_facility_select() {
-    let id = __get_selected_institution_id();
     $.ajax({
-        url: __get_url_to_get_facilities_options_data_for_institution(),
+        url: reverse_url('get-institution-facilities'),
         type: 'POST',
         dataType: 'json',
         headers: {
             'X-CSRFToken': $.cookie('csrftoken')
         },
         data: {
-            institution_id: id
+            institution_id: __get_selected_institution_id()
         },
         success: (ids_and_labels) => {
-            let parent_facility_select = __get_parent_facility_select();
-            parent_facility_select.prop('disabled', false);
-            parent_facility_select.empty();
-            for (const id_label_object_index in (ids_and_labels)) {
-                let hashed_id = ids_and_labels[id_label_object_index][0];
-                let label = ids_and_labels[id_label_object_index][1];
-                parent_facility_select.append(
-                    `<option value="${hashed_id}">${label}</option>`
-                )
-            }
+            update_select_with_options(__get_parent_facility_select(), ids_and_labels);
         },
         error: (error) => {
             window.alert('Під час оновлення даних виникла помилка. ' +
@@ -41,13 +31,8 @@ function set_width_for_all_selects() {
     __get_institution_select().addClass('w-100');
 }
 
-function __get_url_to_get_facilities_options_data_for_institution() {
-    return $('#get_institution_facilities_url').text();
-}
-
-
 function __get_selected_institution_id() {
-    return __get_institution_select().find('option:selected').val();
+    return get_selected_option_for_select(__get_institution_select());
 }
 
 function __get_institution_select() {
