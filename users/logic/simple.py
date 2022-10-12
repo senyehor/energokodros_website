e0831511ce968d2e9d4a524d29be3987e3256914
Yehor Sencha <senyehor@gmail.com>
2022-10-12 12:20:28 +0300

    fix: made protected helper function private

--- users/logic/simple.py
+++ users/logic/simple.py
@@ -4,8 +4,8 @@ from django.http import HttpRequest
 
 
 def remember_user_for_two_week(request: HttpRequest):
-    _remember_user_for_timedelta(request, timedelta(weeks=2))
+    __remember_user_for_timedelta(request, timedelta(weeks=2))
 
 
-def _remember_user_for_timedelta(request: HttpRequest, td: timedelta):
+def __remember_user_for_timedelta(request: HttpRequest, td: timedelta):
     request.session.set_expiry(td)
