4551a711506aed71227070852209da3aabe37052
Yehor Sencha <senyehor@gmail.com>
2022-10-12 14:27:40 +0300

    feat: created reverse_url and update select options funcs

--- static/js/common/utils.js
+++ static/js/common/utils.js
@@ -1,3 +1,18 @@
 function get_selected_option_for_select(select) {
     return select.find('option:selected').val();
 }
+
+function reverse_url(url_name) {
+    return $(`#${url_name}`).text();
+}
+
+function update_select_with_options(select, options) {
+    select.empty();
+    for (const id_label_object_index in (options)) {
+        let hashed_id = options[id_label_object_index][0];
+        let label = options[id_label_object_index][1];
+        select.append(
+            `<option value="${hashed_id}">${label}</option>`
+        )
+    }
+}
