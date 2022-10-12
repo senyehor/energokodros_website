c03b56706b3a8aad3f9242368bb7928bc6584125
Yehor Sencha <senyehor@gmail.com>
2022-10-12 12:19:40 +0300

    feat: migrated to unified way of getting choices

--- institutions/logic/facility_formatting_and_ajax_related.py
+++ institutions/logic/facility_formatting_and_ajax_related.py
@@ -1,31 +1,22 @@
-from builtins import tuple
-from typing import TypeAlias
-
-from django.db.models import QuerySet
 from django.utils.safestring import mark_safe
 
 from institutions.models import Facility
+from utils.common import compose_choices_for_queryset
+from utils.common.model_objects_crypto_related import Choices
 from utils.forms import SecureModelChoiceField
-
-Choices: TypeAlias = list[tuple[str, str]]
 
 
 def compose_formatted_institution_facilities_choices(institution: Facility) -> Choices:
     if not institution.is_root():
         raise ValueError('provided objects is a facility, not institution')
-    return __compose_choices_for_queryset(Facility.objects.get_all_institution_objects(institution))
+    return compose_formatted_facility_descendants_and_itself(institution)
 
 
 def compose_formatted_facility_descendants_and_itself(facility: Facility) -> Choices:
-    return __compose_choices_for_queryset(facility.get_tree(facility))
-
-
-def __compose_choices_for_queryset(qs: QuerySet) -> Choices:
-    choices = SecureModelChoiceFieldWithVerboseFacilityLabeling(
-        queryset=qs,
-        empty_label=None
-    ).choices
-    return [(str(value), label) for value, label in choices]
+    return compose_choices_for_queryset(
+        facility.get_tree(facility),
+        SecureModelChoiceFieldWithVerboseFacilityLabeling
+    )
 
 
 class SecureModelChoiceFieldWithVerboseFacilityLabeling(SecureModelChoiceField):
