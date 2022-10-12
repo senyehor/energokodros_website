bcaf595e91122e51fdac929af18628b709bf8740
Yehor Sencha <senyehor@gmail.com>
2022-10-12 12:19:05 +0300

    feat: created compose_choices_for_queryset to unify process of getting choices for ajax

--- utils/common/model_objects_crypto_related.py
+++ utils/common/model_objects_crypto_related.py
@@ -1,8 +1,20 @@
+from typing import TypeAlias
+
 from django.db.models import Manager, Model, QuerySet
 from django.db.models.base import ModelBase
+from django.forms import ChoiceField
 from django.shortcuts import get_object_or_404
 
-from utils.forms import INT_HIDER, INT_REVEALER
+from utils.forms import INT_HIDER, INT_REVEALER, SecureModelChoiceField
+
+Choices: TypeAlias = list[tuple[str, str]]
+
+
+def compose_choices_for_queryset(
+        qs: QuerySet,
+        choice_field_class: type(ChoiceField) = SecureModelChoiceField) -> Choices:
+    choices = choice_field_class(queryset=qs, empty_label=None).choices
+    return [(str(value), label) for value, label in choices]
 
 
 def get_object_by_hashed_id_or_404(klass: ModelBase | Manager | QuerySet, hashed_id: str) -> Model:
