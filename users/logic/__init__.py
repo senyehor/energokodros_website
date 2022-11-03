e45d7a0bfffd0acb659af8c6714154fdb60aa6bf
Yehor Sencha <senyehor@gmail.com>
2022-11-03 13:17:08 +0200

    fix: corrected imports

--- users/logic/__init__.py
+++ users/logic/__init__.py
@@ -1,2 +1,5 @@
-from .simple import remember_user_for_two_week
+from .simple import (
+    check_role_belongs_to_user, check_role_has_access_for_facility,
+    remember_user_for_two_week,
+)
 from .user_registration_controller import UserRegistrationController
