0cfddd1ae17fd0bfed83f4bd718bbd41e75c3a9f
Yehor Sencha <senyehor@gmail.com>
2022-11-24 14:34:31 +0200

    feat: add roles for facility functionality

--- static/js/institutions/edit_facility.js
+++ static/js/institutions/edit_facility.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
-    style_and_setup_descendants_select();
-    add_descendants_select_on_click_label();
-    __get_descendants_select().change(redirect_on_descendant_click);
+    style_and_setup_selects();
 })
 
-function style_and_setup_descendants_select() {
+function style_and_setup_selects() {
     // disabled is included in django field to ignore possible input
-    // but user can move to facility page by clicking a descendant
+    // but user can move to facility or role page by clicking a descendant
     remove_disable_set_width_100(__get_descendants_select());
+    remove_disable_set_width_100(__get_roles_for_facility_select());
+
+    add_descendants_select_on_click_label();
+    add_roles_on_click_label();
+    
+    __get_descendants_select().change(redirect_on_descendant_click);
 }
 
 function redirect_on_descendant_click() {
@@ -35,11 +39,17 @@ function redirect_on_descendant_click() {
 }
 
 function add_descendants_select_on_click_label() {
-    const on_click_redirects = '' +
-        '<p class="fw-light text-muted">' +
-        'При натисканні на об‘єкт вас перенаправить на його сторінку' +
-        '</p>'
+    const on_click_redirects = generate_muted_p(
+        'При натисканні на об‘єкт вас перенаправить на його сторінку'
+    );
     __get_descendants_label().after(on_click_redirects);
+}
+
+function add_roles_on_click_label() {
+    const on_click_redirects = generate_muted_p(
+        'При натисканні на роль вас перенаправить на її сторінку'
+    );
+    __get_roles_for_facility_select_label().after(on_click_redirects);
 }
 
 function __get_descendants_label() {
@@ -49,4 +59,12 @@ function __get_descendants_label() {
 
 function __get_descendants_select() {
     return $('#id_descendants')
+}
+
+function __get_roles_for_facility_select_label() {
+    return $('#div_id_roles_that_have_access_to_this_facility label');
+}
+
+function __get_roles_for_facility_select() {
+    return $('#id_roles_that_have_access_to_this_facility');
 }
