57a0d4f0f18b693ebe2e49bd59ff3213a2a5b7cd
Yehor Sencha <senyehor@gmail.com>
2022-11-24 10:48:13 +0200

    ref: better typehints

--- administrator/logic/decorators.py
+++ administrator/logic/decorators.py
@@ -1,24 +1,24 @@
 import functools
 import inspect
-from types import FunctionType
+from typing import Callable
 
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 
 
-def admin_rights_required(obj_to_decorate: FunctionType | View):
+def admin_rights_required(obj_to_decorate: Callable | View) -> Callable | View:
     if inspect.isfunction(obj_to_decorate):
         return __admin_right_required_function_decorator(obj_to_decorate)
     return __admin_right_required_class_decorator(obj_to_decorate)
 
 
-def __admin_right_required_class_decorator(_class: View):
+def __admin_right_required_class_decorator(_class: View) -> View:
     _class.dispatch = method_decorator(__admin_right_required_function_decorator)(_class.dispatch)
     return _class
 
 
-def __admin_right_required_function_decorator(function: FunctionType):
+def __admin_right_required_function_decorator(function: Callable) -> Callable:
     @functools.wraps(function)
     def _wrapper(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.is_admin:
