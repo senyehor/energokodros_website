13ea8d7bb3e2a604256acb609b4ae32860bcf6ec
Yehor Sencha <senyehor@gmail.com>
2022-11-26 09:35:43 +0200

    feat: add redirect to role on click

--- static/js/users/edit_user.js
+++ static/js/users/edit_user.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
     style_roles_select();
+    __get_roles_select().change(redirect_to_role_on_click);
 })
+
+function redirect_to_role_on_click() {
+    $.ajax({
+       url: reverse_url('edit-user-role-pk-in-post'),
+        type: 'POST',
+        dataType:'json',
+        headers: {
+            'X-CSRFToken': $.cookie('csrftoken')
+        },
+        data: {
+            'pk': get_selected_option_for_select(__get_roles_select())
+        },
+        success: (data) => {
+            window.open(data.url, '_self');
+        },
+        error: (data) => {
+          add_error_alert(DEFAULT_UNEXPECTED_ERROR_MESSAGE);
+        }
+    });
+}
 
 function style_roles_select() {
     remove_disable_set_width_100(__get_roles_select());
