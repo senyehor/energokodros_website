d95166bd06f03658a8af520bbeb052e3fb7778de
Yehor Sencha <senyehor@gmail.com>
2022-11-29 14:53:10 +0200

    ref: named uniformly

--- utils/common/model_objects_crypto_related.py
+++ utils/common/model_objects_crypto_related.py
@@ -20,8 +20,8 @@ def compose_secure_choices_for_queryset(
     return [(str(value), label) for value, label in choices]
 
 
-def get_object_by_hashed_id_or_404(klass: ModelBase | Manager | QuerySet, hashed_id: str) -> Model:
-    return get_object_or_404(klass, pk=INT_REVEALER(hashed_id))
+def get_object_by_hashed_id_or_404(_class: ModelBase | Manager | QuerySet, hashed_id: str) -> Model:
+    return get_object_or_404(_class, pk=INT_REVEALER(hashed_id))
 
 
 def hash_id(model_object: Model) -> str:
