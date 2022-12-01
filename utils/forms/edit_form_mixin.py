0474bf3ed8ba7678d5fc85b24721591894635f0e
Yehor Sencha <senyehor@gmail.com>
2022-12-01 14:53:51 +0200

    feat: add delete functionality and some typehinting

--- utils/forms/edit_form_mixin.py
+++ utils/forms/edit_form_mixin.py
@@ -3,7 +3,12 @@ from typing import Protocol, runtime_checkable, Union
 from django.contrib import messages
 from django.db.models import Model
 from django.forms import Form
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.utils.translation import gettext as _
 from django.views.generic import UpdateView
+
+from utils.forms.crispy_buttons import DEFAULT_DELETE_VALUE
 
 
 @runtime_checkable
@@ -17,16 +22,28 @@ class EditObjectUpdateViewMixin:
     must be used as a mixin for UpdateView, needs to be first in superclass list
     to correctly override methods
     """
-    edit_success_message: str = None
-    delete_success_message: str = None
+    edit_success_message: str = _('Успішно відредаговано')
+    delete_success_message: str = _('Успішно видалено')
+    no_changes_message: str = _('Ви не внесли жодних змін, тож були перенаправлені')
 
-    def get_context_data(self, **kwargs):
+    def post(
+            self: Union[UpdateView, 'EditObjectUpdateViewMixin'],
+            request: HttpRequest, *args, **kwargs
+    ):
+        if request.POST['submit'] == DEFAULT_DELETE_VALUE:
+            return self.__delete_object()
+        # noinspection PyUnresolvedReferences
+        return super().post(request, *args, **kwargs)
+
+    def get_context_data(self: Union[UpdateView, 'EditObjectUpdateViewMixin'], **kwargs):
         # noinspection PyUnresolvedReferences
         data = super().get_context_data(**kwargs)
         self.fill_from_object_if_needed(data)
         return data
 
-    def fill_from_object_if_needed(self: UpdateView, data: dict):
+    def fill_from_object_if_needed(
+            self: Union[UpdateView, 'EditObjectUpdateViewMixin'], data: dict
+    ):
         form: Form = data['form']
         if isinstance(form, NeedsAdditionalFilling):
             form.additionally_fill(self.object)
@@ -37,5 +54,19 @@ class EditObjectUpdateViewMixin:
                 self.request,
                 self.edit_success_message
             )
-        # noinspection PyUnresolvedReferences
+        else:
+            messages.info(
+                self.request,
+                self.no_changes_message
+            )
         return super().form_valid(form)
+
+    def __delete_object(self: Union[UpdateView, 'EditObjectUpdateViewMixin']):
+        self.get_object().delete()
+        messages.warning(
+            self.request,
+            self.delete_success_message,
+        )
+        # default redirect goes to self.get_success_url(), but is uses self.object,
+        # which has just been deleted, so redirect to .success_url
+        return redirect(self.success_url)
