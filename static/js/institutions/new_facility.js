8609f5a7576596ff2313c6aea342fffce6077f88
Yehor Sencha <senyehor@gmail.com>
2022-12-23 13:50:42 +0200

    feat: created separate file for dynamic facility choice update for institutions

--- static/js/institutions/new_facility.js
+++ static/js/institutions/new_facility.js
@@ -1,25 +1,16 @@
 $(document).ready(function () {
     set_width_for_all_selects();
-    update_parent_facility_select();
-    __get_institution_select().change(update_parent_facility_select);
+    __get_institution_select().change(
+        update_facility_choices_for_chosen_institution_callback(
+            __get_institution_select,
+            update_facility_select
+        )
+    );
+    __get_institution_select().trigger('change');
 })
 
-function update_parent_facility_select() {
-    $.ajax({
-        url: reverse_url('get-institution-facilities'),
-        type: 'POST',
-        dataType: 'json',
-        headers: get_headers_for_ajax_object(),
-        data: {
-            institution_id: __get_selected_institution_id()
-        },
-        success: (ids_and_labels) => {
-            update_select_with_options(__get_parent_facility_select(), ids_and_labels);
-        },
-        error: (error) => {
-            add_error_alert(DEFAULT_UNEXPECTED_ERROR_MESSAGE);
-        }
-    });
+function update_facility_select(ids_and_labels) {
+    update_select_with_options(__get_parent_facility_select(), ids_and_labels);
 }
 
 function set_width_for_all_selects() {
@@ -27,9 +18,6 @@ function set_width_for_all_selects() {
     __get_institution_select().addClass('w-100');
 }
 
-function __get_selected_institution_id() {
-    return get_selected_option_for_select(__get_institution_select());
-}
 
 function __get_institution_select() {
     return $('#id_institution');
