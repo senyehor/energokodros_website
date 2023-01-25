5fac4f7f1047ef93010e91833e88a7811f96d16e
Yehor Sencha <senyehor@gmail.com>
2023-01-25 15:31:56 +0200

    feat: add search form input fill on redirect

--- static/js/common/search_form.js
+++ static/js/common/search_form.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     submit_on_enter(search_form);
     make_input_fit_placeholder();
     setup_clear_filter_button(search_form);
+    fill_search_field_after_redirect_if_search_value_present();
 })
 
 function submit_on_enter(search_form) {
@@ -11,6 +12,14 @@ function submit_on_enter(search_form) {
             search_form.submit();
         }
     })
+}
+
+function fill_search_field_after_redirect_if_search_value_present() {
+    let url = get_current_url();
+    let search_value = url.searchParams.get('search_value');
+    if (search_value) {
+        __get_form_input().val(search_value);
+    }
 }
 
 function make_input_fit_placeholder() {
