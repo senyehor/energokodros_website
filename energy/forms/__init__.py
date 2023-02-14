0a3b9c728d106f1cc4339101ee2a1b7968de9f1a
Yehor Sencha <senyehor@gmail.com>
2023-02-14 14:33:17 +0200

    ref: moved form related to box with sensors set creation to corresponding file along with more verbose naming

--- energy/forms/__init__.py
+++ energy/forms/__init__.py
@@ -1,6 +1,9 @@
-from .box_with_sensors_creation import BoxSensorSetFormset, ChooseInstitutionForm, SensorsFormset
+from .box_with_sensors_creation import (
+    BoxFormNoRelationFields, BoxSensorSetForBoxWithSensorsCreationForm, BoxSensorSetFormset,
+    ChooseInstitutionForm,
+    SensorFormNoRelationFields, SensorsFormset,
+)
 from .energy_consumption_display_page_control_form import EnergyConsumptionDisplayPageControlForm
 from .simple import (
-    BoxFormNoRelationFields, BoxSensorSetForm, SensorForm,
-    SensorFormNoRelationFields,
+    SensorForm,
 )
