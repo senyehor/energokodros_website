8ed1414be28473da5851ec0fa3c5d098494a4358
Yehor Sencha <senyehor@gmail.com>
2023-02-14 14:08:52 +0200

    ref: moved all forms to simple

--- energy/forms/__init__.py
+++ energy/forms/__init__.py
@@ -1,7 +1,6 @@
 from .box_with_sensors_creation import BoxSensorSetFormset, ChooseInstitutionForm, SensorsFormset
 from .energy_consumption_display_page_control_form import EnergyConsumptionDisplayPageControlForm
-from .simple import SensorForm
-from .without_relation_fields_for_box_sensor_set_creation import (
-    BoxFormNoRelationFields, BoxSensorSetFormNoRelationFields,
+from .simple import (
+    BoxFormNoRelationFields, BoxSensorSetFormNoRelationFields, SensorForm,
     SensorFormNoRelationFields,
 )
