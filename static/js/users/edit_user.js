1d565d1f37832f35036580f7bb9607ba01163ce7
Yehor Sencha <senyehor@gmail.com>
2023-02-14 13:33:30 +0200

    ref: cosmetic

--- static/js/users/edit_user.js
+++ static/js/users/edit_user.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     style_roles_select();
+    add_redirect_on_roles_click();
+})
+
+function add_redirect_on_roles_click() {
     __get_roles_select().change(
         redirect_to_selected_object(
             'edit-user-role-pk-in-post'
         )
     );
-})
+}
 
 function style_roles_select() {
     __get_roles_select().removeAttr('disabled');
