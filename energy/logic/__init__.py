8f931819931346fe0c97780e22c56f4e211d6076
Yehor Sencha <senyehor@gmail.com>
2023-02-17 11:41:17 +0200

    feat: removed items from __init__ as they do not fit direct import from logic (energy.logic)

--- energy/logic/__init__.py
+++ energy/logic/__init__.py
@@ -1,5 +1,1 @@
-from .aggregated_consumption import (
-    AggregatedEnergyConsumptionController,
-    EnergyConsumptionExceptionWithMessage,
-)
 from .ajax import get_facilities_formatted_choices_for_user_role
