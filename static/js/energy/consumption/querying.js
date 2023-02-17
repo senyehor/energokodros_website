1063607cf9feb985265853579ffa1b48097dcbfa
Yehor Sencha <senyehor@gmail.com>
2023-02-17 10:56:00 +0200

    fix: made parameter more explicit, adding _pk when needed

--- static/js/energy/consumption/querying.js
+++ static/js/energy/consumption/querying.js
@@ -46,14 +46,15 @@ function get_consumption_data_and_update() {
 
 function __compose_aggregation_query_parameters() {
     let _ = get_selected_option_for_select;
-    let facility = _(__get_facility_select());
+    let facility_pk = _(__get_facility_select());
     let aggregation_interval_seconds = _(__get_aggregation_interval_select());
     let period_start_epoch_seconds = __get_period_start_epoch_seconds();
     let period_end_epoch_seconds = __get_period_end_epoch_seconds();
-    let role = _(__get_role_select());
+    let role_pk = _(__get_role_select());
+    let include_forecast = __get_forecast_checkbox_value();
     let base_parameters = {
-        role: role,
-        facility: facility,
+        role_pk: role_pk,
+        facility_pk: facility_pk,
         period_start_epoch_seconds: period_start_epoch_seconds,
         period_end_epoch_seconds: period_end_epoch_seconds,
         aggregation_interval_seconds: aggregation_interval_seconds
