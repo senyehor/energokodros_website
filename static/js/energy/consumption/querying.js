cdb0f7e91374d133061ffd15f02b965996060e68
Yehor Sencha <senyehor@gmail.com>
2023-03-09 12:37:07 +0200

    feat: add hour filtering method parameters

--- static/js/energy/consumption/querying.js
+++ static/js/energy/consumption/querying.js
@@ -63,6 +63,7 @@ function __compose_aggregation_query_parameters() {
     if (INCLUDE_HOURS_FILTER) {
         base_parameters.hours_filtering_start_hour = _(__get_hours_filtering_start_select());
         base_parameters.hours_filtering_end_hour = _(__get_hours_filtering_end_select());
+        base_parameters.hour_filtering_method = CHOSEN_HOUR_FILTERING_METHOD;
     }
     return base_parameters;
 }
