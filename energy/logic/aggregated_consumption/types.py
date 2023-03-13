36b43043d47681c770e7213d0df27b822e66c167
Yehor Sencha <senyehor@gmail.com>
2023-03-13 14:38:07 +0200

    feat: add new types

--- energy/logic/aggregated_consumption/types.py
+++ energy/logic/aggregated_consumption/types.py
@@ -6,12 +6,17 @@ from typing import TypeAlias
 RawConsumptionForecast: TypeAlias = float
 RawConsumptionTime: TypeAlias = datetime | str
 RawConsumptionValue: TypeAlias = Decimal
+RawTotalConsumption: TypeAlias = Decimal
 
 FormattedConsumptionTime: TypeAlias = str
 FormattedConsumptionValue: TypeAlias = str
 FormattedConsumptionForecast: TypeAlias = str
+FormattedTotalConsumption: TypeAlias = str
 
 RawAggregatedConsumptionData = list[tuple[RawConsumptionTime, RawConsumptionValue]]
+RawAggregatedConsumptionDataWithRawTotalConsumption = tuple[
+    RawAggregatedConsumptionData, RawTotalConsumption
+]
 AggregatedConsumptionData: TypeAlias = \
     list[
         tuple[FormattedConsumptionTime, FormattedConsumptionValue]
