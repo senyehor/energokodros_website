4ff958c64d0d287d766e0dad7bf1acb7407a0daf
Yehor Sencha <senyehor@gmail.com>
2023-05-01 11:50:10 +0300

    feat: create run aggregation foundation

---
+++ energy/logic/run_aggregation/models_and_constants.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+from enum import Enum
+
+LAST_TIME_AGGREGATION_WAS_RUN_FORMAT = 'TODO'
+
+MAX_AGGREGATION_START_TIME = timedelta(seconds=10)
+
+
+class AggregationStates(Enum, str):
+    IDLE = 'idle'
+    RUNNING = 'running'
