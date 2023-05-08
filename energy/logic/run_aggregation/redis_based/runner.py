b3bb73d88edb6a18b09d851521e937ee25d20b0d
Yehor Sencha <senyehor@gmail.com>
2023-05-08 07:45:53 +0300

    feat: add aggregation start checking and general arch improvements

--- energy/logic/run_aggregation/redis_based/runner.py
+++ energy/logic/run_aggregation/redis_based/runner.py
@@ -1,12 +1,15 @@
-from datetime import datetime, timedelta
-
 from redis.client import Redis
 
-from energy.logic.run_aggregation.bases import AggregationRunnerBase, AggregationStartedCheckerBase
-from energy.logic.run_aggregation.exceptions import AggregationAlreadyRunning
+from energy.logic.run_aggregation.bases import AggregationRunnerBase
+from energy.logic.run_aggregation.exceptions import (
+    AggregationAlreadyRunning,
+    AggregationDidNotStart,
+)
 from energy.logic.run_aggregation.models_and_constants import AggregationStates
+from energy.logic.run_aggregation.redis_based.aggregation_started_checker import \
+    AggregationStartedChecker
 from energy.logic.run_aggregation.redis_based.exceptions import (
-    AggregationStartedConfirmationMessageInvalid, InvalidStartAggregationRequestReceiverCount,
+    InvalidStartAggregationRequestReceiverCount,
 )
 from energy.logic.run_aggregation.redis_based.state_retriever import RedisAggregationStateRetriever
 
@@ -17,18 +20,23 @@ class RedisAggregationRunner(AggregationRunnerBase):
     def __init__(
             self, r: Redis, start_aggregation_message: str,
             start_aggregation_channel: str, state_retriever: RedisAggregationStateRetriever,
-            aggregation_started_checker: 'AggregationStartedChecker'
+            aggregation_started_checker: AggregationStartedChecker
     ):
         self.__r = r
         self.__start_aggregation_message = start_aggregation_message
         self.__channel = start_aggregation_channel
         self.__state_retriever = state_retriever
+        self.__aggregation_started_checker = aggregation_started_checker
 
     def run_aggregation(self):
-        state = self.__state_retriever.get_state()
-        if state == AggregationStates.RUNNING:
-            raise AggregationAlreadyRunning
+        self.__ensure_aggregation_is_not_running()
         self.__send_start_aggregation_message()
+        self.__ensure_aggregation_started()
+
+    def __ensure_aggregation_started(self):
+        if self.__aggregation_started_checker.check_aggregation_started():
+            return
+        raise AggregationDidNotStart
 
     def __send_start_aggregation_message(self):
         receiver_count = self.__r.publish(
@@ -37,33 +45,11 @@ class RedisAggregationRunner(AggregationRunnerBase):
         )
         self.__ensure_start_aggregation_message_receiver_count_is_correct(receiver_count)
 
+    def __ensure_aggregation_is_not_running(self):
+        state = self.__state_retriever.get_state()
+        if state == AggregationStates.RUNNING:
+            raise AggregationAlreadyRunning
+
     def __ensure_start_aggregation_message_receiver_count_is_correct(self, receiver_count: int):
         if receiver_count != self.__EXPECTED_START_AGGREGATION_REQUEST_RECEIVER_COUNT:
             raise InvalidStartAggregationRequestReceiverCount(receiver_count)
-
-
-class AggregationStartedChecker(AggregationStartedCheckerBase):
-    def __init__(
-            self, aggregation_started_confirmation_channel: str,
-            aggregation_started_confirmation_message: str, r: Redis,
-            max_start_time: timedelta
-    ):
-        self.__channel = r.pubsub()
-        self.__channel.subscribe(aggregation_started_confirmation_channel)
-        self.__confirmation_message = aggregation_started_confirmation_message
-        self.__max_start_time = max_start_time
-
-    def check_aggregation_started(self) -> bool:
-        max_wait_time = datetime.now() + self.__max_start_time
-        while datetime.now() < max_wait_time:
-            if self.__try_get_aggregation_started_message():
-                return True
-        return False
-
-    def __try_get_aggregation_started_message(self):
-        if message := self.__channel.get_message():
-            data = message.get('data').decode()
-            if data == self.__confirmation_message:
-                return True
-            raise AggregationStartedConfirmationMessageInvalid
-        return False
