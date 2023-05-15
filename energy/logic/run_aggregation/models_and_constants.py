d483be597878eeba054f63474f5e2649a5ee04d8
Yehor Sencha <senyehor@gmail.com>
2023-05-15 10:06:32 +0300

    fix: correct time format

--- energy/logic/run_aggregation/models_and_constants.py
+++ energy/logic/run_aggregation/models_and_constants.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-LAST_TIME_AGGREGATION_WAS_RUN_FORMAT = '%Y-%m-%dT%H:%M:%S.%f%z'
+LAST_TIME_AGGREGATION_WAS_RUN_FORMAT = '%Y-%m-%d %H:%M:%S %z'
 
 
 class AggregationStates(str, Enum):
