2676af65e63398a15917ade479887a8c0cee24f9
senyehor <senyehor@gmail.com>
2023-06-20 12:50:04 +0300

    added pylintrc; added Institution, Object and AccessLevel models

--- institutions/models.py
+++ institutions/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Institution(models.Model):
+    institution_id = models.AutoField(
+        primary_key=True,
+        db_column='institution_id'
+    )
+    institution_name = models.CharField(
+        max_length=1000,
+        null=False,
+        blank=False
+    )
+    institution_description = models.TextField(null=False, blank=False)
+
+    class Meta:
+        db_table = 'institutions'
+
+
+class AccessLevel(models.Model):
+    access_level_id = models.AutoField(primary_key=True)
+    level_def = models.IntegerField(unique=True, null=False, blank=False)
+    level_description = models.TextField(blank=False, null=False)
+
+    class Meta:
+        db_table = 'access_levels'
+
+
+class Object(models.Model):
+    object_id = models.AutoField(primary_key=True)
+    institution = models.ForeignKey(Institution, models.CASCADE, null=True)
+    object_name = models.CharField(max_length=1000, blank=True, null=False)
+    object_description = models.TextField(blank=True, null=True)
+    parent = models.ForeignKey('self', models.CASCADE, null=True, blank=True)
+    access_level = models.ForeignKey('AccessLevel', models.RESTRICT, null=False, blank=False)
+
+    class Meta:
+        db_table = 'objects'
