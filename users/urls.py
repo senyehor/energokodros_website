012101fd68153878eacd86629587ccf99515d5eb
Yehor Sencha <senyehor@gmail.com>
2023-06-29 12:17:02 +0300

    fixed wrong url mapping for user successful registration and removed optional trailing spaces, as django adds it on it`s own

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,18 @@
-from django.urls import path, include, re_path
+from django.urls import include, path, re_path
 
 from users import views
 
 # using /? to override some of django.contrib.auth.urls
 overridden_account_patterns = [
-    re_path('login/?', views.LoginView.as_view(template_name='registration/login.html')),
+    re_path('login/', views.LoginView.as_view(template_name='registration/login.html')),
 ]
 
 urlpatterns = overridden_account_patterns + [
-    re_path('registration/?', views.CreateUserRegistrationRequest.as_view(), name='register'),
+    re_path(
+        'registration/',
+        views.CreateUserRegistrationRequest.as_view(),
+        name='register'
+    ),
     path('', include('django.contrib.auth.urls')),
     path(
         'successfully-created-registration-request',
