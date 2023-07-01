dab7d925fd788666a8f3d5f70d4c398480b2c544
Yehor Sencha <senyehor@gmail.com>
2023-07-01 11:55:19 +0300

    little paths refactoring, as re_path is no longer needed + added / at the end of every path

--- users/urls.py
+++ users/urls.py
@@ -1,22 +1,23 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 from users import views
 
 # using /? to override some of django.contrib.auth.urls
 overridden_account_patterns = [
-    re_path('login/', views.LoginView.as_view(template_name='registration/login.html')),
+    path('login/', views.LoginView.as_view(template_name='registration/login.html')),
 ]
 
 urlpatterns = overridden_account_patterns + [
-    re_path(
+    path(
         'registration/',
         views.CreateUserRegistrationRequest.as_view(),
         name='register'
     ),
     path('', include('django.contrib.auth.urls')),
     path(
-        'successfully-created-registration-request',
+        'successfully-created-registration-request/',
         views.successfully_created_registration_request,
         name='successfully_created_registration_request'
     ),
+    path('confirm-email/', views.confirm_email, name='confirm_email')
 ]
