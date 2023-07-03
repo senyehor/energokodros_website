f1f0f4e462f8c4446f398ccc50cea23e05f76101
Yehor Sencha <senyehor@gmail.com>
2023-07-03 12:09:48 +0300

    created utils module with custom url types for encrypting ints and strings

---
+++ utils/crypto.py
@@ -0,0 +1,63 @@
+import string
+
+from django.urls import register_converter
+from hashids import Hashids
+
+from energokodros.settings import MIN_HASH_LENGTH, SECRET_KEY
+
+# here we are setting custom alphabet to exclude all unnecessary symbols (/,% etc)
+# and to simplify checking by regex, as by default __HASHER._alphabet does not return
+# correct alphabet for regex (do not know why)
+__ALPHABET = string.ascii_lowercase + string.digits
+
+_HASHER = Hashids(salt=SECRET_KEY, min_length=MIN_HASH_LENGTH, alphabet=__ALPHABET)
+
+_REGEX = f'[{__ALPHABET}]' + '{%i,}' % MIN_HASH_LENGTH  # noqa pylint: disable=C0209
+
+
+class IntHasher:
+    regex = _REGEX
+
+    def to_python(self, value: str) -> int:  # noqa
+        return reveal_int(value)
+
+    def to_url(self, value: int) -> str:  # noqa
+        return hide_int(value)
+
+
+class StringHasher:
+    regex = _REGEX
+
+    def to_python(self, value: str) -> str:  # noqa
+        return reveal_str(value)
+
+    def to_url(self, value: str) -> str:  # noqa
+        return hide_str(value)
+
+
+register_converter(IntHasher, 'hashed_int')
+register_converter(StringHasher, 'hashed_string')
+
+
+def hide_int(val: int) -> str:
+    if val < 0:
+        raise ValueError('only positive integers are allowed')
+    return _HASHER.encode(val)
+
+
+def reveal_int(val: str) -> int:
+    res = _HASHER.decode(val)
+    if len(res) != 1:
+        raise ValueError('one integer return expected, probably wrong input')
+    return res[0]
+
+
+def hide_str(val: str) -> str:
+    return _HASHER.encode(*(ord(c) for c in val))
+
+
+def reveal_str(val: str) -> str:
+    try:
+        return ''.join(chr(c) for c in _HASHER.decode(val))
+    except ValueError as e:
+        raise ValueError('can not transform to char, probably wrong input') from e
