2b7e580044e89519ca9874402ae13fa3ea2069dc
Yehor Sencha <senyehor@gmail.com>
2023-07-06 11:16:08 +0300

    created SecureModelChoiceField that does not expose ids

---
+++ utils/forms.py
@@ -0,0 +1,13 @@
+from django.db import models as db_models
+from django.forms import models
+
+from utils.crypto import hide_int, reveal_int
+
+
+class SecureModelChoiceField(models.ModelChoiceField):
+
+    def prepare_value(self, value: db_models.Model) -> str:
+        return hide_int(value.pk)
+
+    def to_python(self, value: str) -> db_models.Model:
+        return super().to_python(reveal_int(value))
