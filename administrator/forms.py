15ea045fcf30f1d6ff571ac9db1912d691c0518b
Yehor Sencha <senyehor@gmail.com>
2023-07-06 11:05:23 +0300

    set rows count for text input, as they were too high

--- administrator/forms.py
+++ administrator/forms.py
@@ -17,13 +17,13 @@ class UserRegistrationRequestsDecisionForm(forms.Form):
     )
     position = forms.CharField(
         max_length=100,
-        widget=forms.Textarea,
+        widget=forms.Textarea({'rows': 1}),
         required=True,
         label=_('Уведіть позицію')
     )
     message = forms.CharField(
         max_length=255,
-        widget=forms.Textarea,
+        widget=forms.Textarea({'rows': 4}),
         required=False,
         label=_('Залиште опціональне повідомлення для користувача')
     )
