4d752ced0adcb144af0af85b5fb55f5bcca4e0da
Illia Volkov <kuza617@gmail.com>
2023-07-12 15:35:58 +0300

    moved is_admin(request) from user to administrator app

--- administrator/logic.py
+++ administrator/logic.py
@@ -66,7 +66,7 @@ class UserRoleApplicationReviewController:
         )
 
 
-def _get_message_for_role_application(application_id: int):
+def _get_message_for_role_application(application_id: int) -> UserRoleApplication:
     # as we use SecureModelChoiceField we get request_id hashed,
     # so converting it to int back
     return get_object_or_404(UserRoleApplication, pk=application_id)
@@ -78,3 +78,7 @@ def _check_user_has_no_roles(user: User) -> bool:
 
 def _get_applications_from_users_who_confirmed_email_ordered() -> QuerySet:
     return UserRoleApplication.objects.all().filter(user__is_active=True).order_by('-pk')
+
+
+def _is_admin(request: HttpRequest):
+    return request.user.is_authenticated and request.user.is_admin  # noqa
