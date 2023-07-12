a21b7f07274608ffa08760f2a04a6cfc9f9d0e8a
Yehor Sencha <senyehor@gmail.com>
2023-07-12 13:56:24 +0300

    added smtp error logging

--- utils/common.py
+++ utils/common.py
@@ -1,14 +1,25 @@
+import logging
 from smtplib import SMTPException
 
 from django.contrib import messages
 from django.core.mail import EmailMultiAlternatives
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
+
+logger = logging.getLogger(__name__)
 
 
 def try_send_email_add_warning_if_failed(
         request: HttpRequest, email: str, subject: str, message: str):
-    if send_html_email(email, subject, message):
+    html = render_to_string(
+        'email/regular_email.html',
+        context={
+            'header': subject,
+            'text':   message
+        }
+    )
+    if send_html_email(email, subject, html):
         messages.warning(request, _(f'Не вдалося надіслати повідомлення на пошту {email}'))
 
 
@@ -24,6 +35,11 @@ def send_html_email(email: str, subject: str, html: str) -> bool:
     msg.content_subtype = 'html'
     try:
         msg.send()
-    except SMTPException:
+    except SMTPException as e:
+        logging.error(
+            'failed to send email to %s with error: %s',
+            email,  # pylint: disable=C0209
+            e
+        )
         return False
     return True
