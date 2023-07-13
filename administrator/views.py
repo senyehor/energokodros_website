ebac8d4cd3d3d0e2ef87623c388a4c1dcf4e5b4c
Yehor Sencha <senyehor@gmail.com>
2023-07-13 14:44:30 +0300

    minor changes due to dependencies changes

--- administrator/views.py
+++ administrator/views.py
@@ -5,11 +5,12 @@ from django.views.generic import FormView, ListView
 
 from administrator.decorators import admin_rights_required
 from administrator.forms import UserRoleApplicationRequestsDecisionForm
-from administrator.logic import (
-    _check_user_has_no_roles,
-    _get_applications_from_users_who_confirmed_email_ordered,
-    UserRoleApplicationReviewController,
+from administrator.logic.helper_functions import (
+    check_user_has_no_roles,
+    get_applications_from_users_who_confirmed_email_ordered,
 )
+from administrator.logic.user_role_application_review_controller import \
+    UserRoleApplicationReviewController
 from users.models import UserRoleApplication
 
 
@@ -24,7 +25,7 @@ def admin_page(request: HttpRequest):
 @admin_rights_required
 class UserRoleApplicationsList(ListView):
     # pagination without ordering might be inconsistent - from docs
-    queryset = _get_applications_from_users_who_confirmed_email_ordered()
+    queryset = get_applications_from_users_who_confirmed_email_ordered()
     context_object_name = 'applications'
     paginate_by = 10
     template_name = 'administrator/users_roles_applications.html'
@@ -33,13 +34,13 @@ class UserRoleApplicationsList(ListView):
 @admin_rights_required
 class UserRoleApplicationDecision(FormView):
     http_method_names = ['get', 'post']  # removing PUT method that is in FormView by default
-    template_name = 'administrator/user_role_application.html'
+    template_name = 'administrator/user_role_application_decision.html'
     form_class = UserRoleApplicationRequestsDecisionForm
     success_url = reverse_lazy('users_roles_applications')
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['user_has_no_roles'] = _check_user_has_no_roles(self.__get_application_request().user)
+        ctx['user_has_no_roles'] = check_user_has_no_roles(self.__get_application_request().user)
         return ctx
 
     def get_form(self, form_class=None):
@@ -49,22 +50,17 @@ class UserRoleApplicationDecision(FormView):
 
     def post(self, request: HttpRequest, *args, **kwargs):
         form = self.form_class(request.POST or None)
-        self.controller = UserRoleApplicationReviewController(  # noqa
+        self.controller = UserRoleApplicationReviewController(
             form,
             self.__get_application_request(),
             self.request,
+            self.application_approved
         )
-        return self.form_valid(form) if form.is_valid() else self.form_invalid(form)
-
-    def form_invalid(self, form: UserRoleApplicationRequestsDecisionForm):
-        if not (is_approved := self.application_approved):
-            # case when admin decided to decline application,
-            # so we do not care about any form validation
-            self.controller.process_depending_on_decision(is_approved)
-        return super().form_invalid(form)
+        valid = self.controller.validate_form()
+        return self.form_valid(form) if valid else self.form_invalid(form)
 
     def form_valid(self, form: UserRoleApplicationRequestsDecisionForm):
-        self.controller.process_depending_on_decision(self.application_approved)
+        self.controller.process_depending_on_decision()
         return redirect(reverse('users_roles_applications'))
 
     def __get_application_request(self):
@@ -73,3 +69,7 @@ class UserRoleApplicationDecision(FormView):
     @property
     def application_approved(self):
         return self.request.POST['decision'] == 'accept'
+
+    @property
+    def application_declined(self):
+        return not self.application_approved
