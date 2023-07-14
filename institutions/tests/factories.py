5a36ebac70ecfa982cc13eb59cc13da26c93b7b3
Illia Volkov <kuza617@gmail.com>
2023-07-14 11:26:17 +0300

    fix due to models fields renaming

--- institutions/tests/factories.py
+++ institutions/tests/factories.py
@@ -3,24 +3,24 @@ from factory import django
 
 
 class InstitutionFactory(django.DjangoModelFactory):
-    institution_name = factory.Sequence(lambda n: f'institution {n}')
-    institution_description = factory.Sequence(lambda n: f'institution {n} description')
+    name = factory.Sequence(lambda n: f'institution {n}')
+    description = factory.Sequence(lambda n: f'institution {n} description')
 
     class Meta:
         model = 'institutions.Institution'
 
 
 class AccessLevelFactory(django.DjangoModelFactory):
-    level_def = factory.Sequence(lambda n: n)
-    level_description = factory.Sequence(lambda n: f'level {n} description')
+    code = factory.Sequence(lambda n: n)
+    description = factory.Sequence(lambda n: f'level {n} description')
 
     class Meta:
         model = 'institutions.AccessLevel'
 
 
 class ObjectFactory(django.DjangoModelFactory):
-    object_name = factory.Sequence(lambda n: f'object {n}')
-    object_description = factory.Sequence(lambda n: f'object {n} description')
+    name = factory.Sequence(lambda n: f'object {n}')
+    description = factory.Sequence(lambda n: f'object {n} description')
 
     class Meta:
         model = 'institutions.Object'
