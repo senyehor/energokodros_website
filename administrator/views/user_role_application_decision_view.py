792f996dbdf7516a7bdd2f7bf09e71cbbf712c07
Yehor Sencha <senyehor@gmail.com>
2023-07-20 14:03:44 +0300

    restructured administrator views to module

--- administrator/views/user_role_application_decision_view.py
+++ administrator/views/user_role_application_decision_view.py
@@ -1,34 +1,12 @@
 from django.http import HttpRequest
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import FormView, ListView
+from django.views.generic import FormView
 
 from administrator.decorators import admin_rights_required
 from administrator.forms import UserRoleApplicationRequestsDecisionForm
-from administrator.logic import (
-    check_user_has_no_roles,
-    get_applications_from_users_who_confirmed_email_ordered,
-    UserRoleApplicationReviewController
-)
-from energokodros.settings import DEFAULT_PAGINATE_BY
+from administrator.logic import check_user_has_no_roles, UserRoleApplicationReviewController
 from users.models import UserRoleApplication
-from utils.filters import QuerySetFieldsIcontainsFilterPkOrderedMixin
-
-
-@admin_rights_required
-def admin_page(request: HttpRequest):
-    return render(
-        request,
-        'administrator/administrator.html',
-    )
-
-
-@admin_rights_required
-class UserRoleApplicationsListView(QuerySetFieldsIcontainsFilterPkOrderedMixin, ListView):
-    queryset = get_applications_from_users_who_confirmed_email_ordered()
-    filter_fields = ('user__full_name', 'user__email', 'institution__name')
-    paginate_by = DEFAULT_PAGINATE_BY
-    template_name = 'administrator/users_roles_applications.html'
 
 
 @admin_rights_required
