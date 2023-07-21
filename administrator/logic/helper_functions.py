b49531b8047c3d7b562890d9d252c2c0be7544e5
Illia Volkov <kuza617@gmail.com>
2023-07-21 12:44:33 +0300

    renamed as function no longer orders by pk

--- administrator/logic/helper_functions.py
+++ administrator/logic/helper_functions.py
@@ -2,15 +2,14 @@ from django.db.models import QuerySet
 from django.http import HttpRequest
 
 from users.models import User, UserRoleApplication
-from utils.common import all_object_pk_ordered
 
 
 def check_user_has_no_roles(user: User) -> bool:
     return not user.roles.exists()
 
 
-def get_applications_from_users_who_confirmed_email_ordered() -> QuerySet:
-    return all_object_pk_ordered(UserRoleApplication).filter(user__is_active=True)
+def get_applications_from_users_who_confirmed_email() -> QuerySet:
+    return UserRoleApplication.objects.filter(user__is_active=True)
 
 
 def is_admin(request: HttpRequest) -> bool:
