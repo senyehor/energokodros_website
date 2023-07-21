c853f292086e83beb6467f938cbf59056ab10a6f
Yehor Sencha <senyehor@gmail.com>
2023-07-21 14:08:36 +0300

    improved search_form appearance and refactored it`s js file

--- static/js/common/search_form.js
+++ static/js/common/search_form.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     let search_form = $('#searchForm');
     submit_on_enter(search_form);
-    make_input_fit_placeholder(search_form);
+    make_input_fit_placeholder();
     setup_clear_filter_button(search_form);
 })
 
@@ -13,18 +13,22 @@ function submit_on_enter(search_form) {
     })
 }
 
-function make_input_fit_placeholder(search_form) {
-    let input = search_form.children(':input');
+function make_input_fit_placeholder() {
+    let input = __get_form_input();
     input.attr('size', input.attr('placeholder').length);
 }
 
 function setup_clear_filter_button(search_form) {
-    let input = search_form.children(':input');
+    let input = __get_form_input()
     $('#clearFilter').click(
         function () {
             input.value = '';
             search_form.submit();
         }
     )
+}
+
+function __get_form_input() {
+    return $('#searchFormInput');
 }
 
