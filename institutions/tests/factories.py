18b462b623d478cdf213bd4bda9e84a1ba14d754
Illia Volkov <kuza617@gmail.com>
2023-07-25 14:26:31 +0300

    !NOT WORKING! created facility factory

--- institutions/tests/factories.py
+++ institutions/tests/factories.py
@@ -1,26 +1,17 @@
 import factory
 from factory import django
 
+from institutions.models import Facility
 
-class InstitutionFactory(django.DjangoModelFactory):
+
+class FacilityFactory(django.DjangoModelFactory):
+    """by default creates root node"""
     name = factory.Sequence(lambda n: f'institution {n}')
     description = factory.Sequence(lambda n: f'institution {n} description')
 
-    class Meta:
-        model = 'institutions.Institution'
-
-
-class AccessLevelFactory(django.DjangoModelFactory):
-    code = factory.Sequence(lambda n: n)
-    description = factory.Sequence(lambda n: f'level {n} description')
+    @classmethod
+    def _create(cls, model_class: 'Facility', *args, **kwargs):
+        return model_class.add_root(**kwargs)
 
     class Meta:
-        model = 'institutions.AccessLevel'
-
-
-class ObjectFactory(django.DjangoModelFactory):
-    name = factory.Sequence(lambda n: f'object {n}')
-    description = factory.Sequence(lambda n: f'object {n} description')
-
-    class Meta:
-        model = 'institutions.Object'
+        model = Facility
