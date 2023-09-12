c7c2e205824e3407b8d6cc941d5a4eea776dae7b
Yehor Sencha <senyehor@gmail.com>
2023-09-12 09:05:38 +0300

    feat: new architecture using new types

--- energy/logic/consumption_report/docx.py
+++ energy/logic/consumption_report/docx.py
@@ -3,19 +3,13 @@ from datetime import datetime
 from io import BytesIO
 
 import matplotlib.pyplot as plt
-import numpy as np
 from django.utils.translation import gettext as _
 from docx import Document
 
-from energy.logic.aggregated_consumption.forecast import (
-    KINDERGARTEN_CONSUMPTION_PER_WEEKEND_DAY,
-    KINDERGARTEN_CONSUMPTION_PER_WORKING_DAY,
-)
-from energy.logic.aggregated_consumption.models import AggregationIntervalSeconds
 from energy.logic.aggregated_consumption.parameters import CommonQueryParameters
-from energy.logic.aggregated_consumption.simple import check_institution_is_kindergarten_28
 from energy.logic.aggregated_consumption.types import (
-    Consumption, TotalConsumption,
+    ConsumptionRawAndFormatted, ConsumptionRawAndFormattedWithForecastRawAndFormatted,
+    TotalConsumption,
 )
 from energy.logic.consumption_report.aggregation_interval_verbose import \
     get_aggregation_interval_verbose
@@ -28,11 +22,13 @@ class ReportCreator:
     __CONSUMPTION_TABLE_ADDITIONAL_ROWS_COUNT = 2
 
     def __init__(
-            self, energy_consumption: Consumption,
+            self, energy_consumption_raw_and_formatted_with_optional_forecast:
+            ConsumptionRawAndFormattedWithForecastRawAndFormatted | ConsumptionRawAndFormatted,
             total_consumption: TotalConsumption,
             query_parameters: CommonQueryParameters
     ):
-        self.__energy_consumption = energy_consumption
+        self.__energy_consumption_raw_and_formatted_with_optional_forecast = \
+            energy_consumption_raw_and_formatted_with_optional_forecast
         self.__query_parameters = query_parameters
         self.__total_consumption = total_consumption
         self.__report = Document()
@@ -84,7 +80,10 @@ class ReportCreator:
         table_name.add_run(
             _('Відомість фактичних показників споживання електроенергії')
         ).bold = True
-        energy_records = self.__energy_consumption
+        energy_records = [
+            record.formatted_consumption_record for record in
+            self.__energy_consumption_raw_and_formatted_with_optional_forecast
+        ]
         energy_records_count = len(energy_records)
         table = self.__report.add_table(
             cols=self.__CONSUMPTION_TABLE_COLUMNS_COUNT,
@@ -96,8 +95,8 @@ class ReportCreator:
         header_row.cells[1].text = _('Показник електроспоживання, кВт/год')
 
         for record_number, consumption in enumerate(energy_records):
-            table.rows[record_number + 1].cells[0].text = consumption[0]
-            table.rows[record_number + 1].cells[1].text = consumption[1]
+            table.rows[record_number + 1].cells[0].text = consumption.value
+            table.rows[record_number + 1].cells[1].text = consumption.time
 
         total_row = table.rows[-1]
         total_text = total_row.cells[0].add_paragraph()
@@ -106,36 +105,26 @@ class ReportCreator:
         total_value.add_run(self.__total_consumption).bold = True
 
     def __add_chart(self):
-        interval_index, value_index = 0, 1
-        # todo make get raw data where needed + redesign controller
-        labels = [str(consumption_record[interval_index]) for consumption_record in
-                  self.__energy_consumption]
-        values = np.array(
-            [float(consumption_record[value_index]) for consumption_record in
-             self.__energy_consumption]
-        )
-        fig, ax = plt.subplots()
+        labels = [
+            record.formatted_consumption_record.time for record in
+            self.__energy_consumption_raw_and_formatted_with_optional_forecast
+        ]
+        values = [
+            raw_and_formatted_record.raw_consumption_record.value for raw_and_formatted_record in
+            self.__energy_consumption_raw_and_formatted_with_optional_forecast
+        ]
+        _, ax = plt.subplots()
         ax.bar(
             labels, values, color=(1, 0.39, 0.51), hatch='//', label=_('Фактичне споживання'),
             zorder=4, alpha=0.5
         )
-        is_kindergarten_28 = check_institution_is_kindergarten_28(
-            self.__query_parameters.facility_to_get_consumption_for_or_all_descendants_if_any
-        )
-        if self.__query_parameters.aggregation_interval == AggregationIntervalSeconds.ONE_DAY \
-                and is_kindergarten_28:
-            consumption_days = [
-                consumption_record[interval_index] for consumption_record in
-                self.__energy_consumption
+        if self.__query_parameters.include_forecast:
+            consumption_forecast = [
+                record.forecast_raw_and_formatted.raw
+                for record in self.__energy_consumption_raw_and_formatted_with_optional_forecast
             ]
-            expected_consumption = np.array(
-                [
-                    KINDERGARTEN_CONSUMPTION_PER_WORKING_DAY if day.isoweekday() in range(1, 5)
-                    else KINDERGARTEN_CONSUMPTION_PER_WEEKEND_DAY for day in consumption_days
-                ]
-            )
             ax.bar(
-                labels, expected_consumption, color=(0.5, 1, 0), hatch=r'\\',
+                labels, consumption_forecast, color=(0.5, 1, 0), hatch=r'\\',
                 label=_('Ліміт електроспоживання'), alpha=0.5, zorder=2
             )
         ax.legend()
