35c5db09ae75c628334af2678885cfc9a2703f7c
Yehor Sencha <senyehor@gmail.com>
2023-09-20 12:30:13 +0300

    ref: move aggregation interval to verbose mapping inside get interval verbose function as it is not used anywhere else

--- energy/logic/consumption_report/verbose_constants.py
+++ energy/logic/consumption_report/verbose_constants.py
@@ -3,16 +3,15 @@ from django.utils.translation import gettext as _
 from energy.logic.aggregated_consumption.models import AggregationIntervalSeconds
 from energy.logic.aggregated_consumption.parameters import OneHourAggregationIntervalQueryParameters
 
-__AGGREGATION_INTERVALS_VERBOSE = {
-    AggregationIntervalSeconds.ONE_HOUR:  _('година'),
-    AggregationIntervalSeconds.ONE_DAY:   _('доба'),
-    AggregationIntervalSeconds.ONE_WEEK:  _('тиждень'),
-    AggregationIntervalSeconds.ONE_MONTH: _('місяць'),
-    AggregationIntervalSeconds.ONE_YEAR:  _('рік'),
-}
-
 
 def get_aggregation_interval_verbose(interval: AggregationIntervalSeconds) -> str:
+    __AGGREGATION_INTERVALS_VERBOSE = {
+        AggregationIntervalSeconds.ONE_HOUR:  _('година'),
+        AggregationIntervalSeconds.ONE_DAY:   _('доба'),
+        AggregationIntervalSeconds.ONE_WEEK:  _('тиждень'),
+        AggregationIntervalSeconds.ONE_MONTH: _('місяць'),
+        AggregationIntervalSeconds.ONE_YEAR:  _('рік'),
+    }
     return __AGGREGATION_INTERVALS_VERBOSE[interval]
 
 
