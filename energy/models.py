f5284f4808b524bf33fa49b1baff1e829fd61c99
Yehor Sencha <senyehor@gmail.com>
2023-10-01 15:19:27 +0300

    feat: created energy models

--- energy/models.py
+++ energy/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from institutions.models import Facility
+
+
+class Box(models.Model):
+    box_id = models.AutoField(primary_key=True)
+    box_number = models.CharField(max_length=256, blank=False, null=False)
+    box_description = models.TextField(blank=False, null=False)
+
+    class Meta:
+        db_table = 'boxes'
+
+
+class Sensor(models.Model):
+    sensor_id = models.AutoField(primary_key=True)
+    sensor_number = models.IntegerField(blank=False, null=False)
+    sensor_description = models.TextField(blank=False, null=False)
+
+    class Meta:
+        db_table = 'sensors'
+
+
+class BoxSensorsSet(models.Model):
+    boxes_set_id = models.AutoField(primary_key=True)
+    box = models.ForeignKey(Box, models.CASCADE, null=False)
+    sensor = models.OneToOneField(Sensor, models.RESTRICT, null=False)
+    facility = models.OneToOneField(Facility, models.CASCADE, blank=True, null=True)
+    line_name = models.CharField(max_length=1000, blank=False, null=False)
+    date_set_in = models.DateTimeField(blank=True, null=True)
+    sensor_number = models.IntegerField()
+
+    class Meta:
+        db_table = 'boxes_sets'
