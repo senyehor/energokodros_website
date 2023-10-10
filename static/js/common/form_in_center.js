bc8a088d082f051cc2cafdd0c5647eb849304dfb
Yehor Sencha <senyehor@gmail.com>
2023-10-10 10:30:38 +0300

    feat: add deletion confirmation

--- static/js/common/form_in_center.js
+++ static/js/common/form_in_center.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     add_spacing_between_form_group();
+    add_confirmation_to_all_delete_buttons();
 })
 
 function add_spacing_between_form_group() {
@@ -8,4 +9,21 @@ function add_spacing_between_form_group() {
             $(this).addClass('pb-2');
         }
     )
+}
+
+function add_confirmation_to_all_delete_buttons() {
+    const confirm_deletion_text = 'Підтвердьте видалення';
+    let delete_buttons = $('button[type="submit"][value="delete"]');
+    delete_buttons.each(
+        (index, button) => {
+            $(button).on('click',
+                (event) => {
+                    if (window.confirm(confirm_deletion_text)) {
+                        return;
+                    }
+                    event.preventDefault();
+                    event.stopPropagation();
+                })
+        }
+    );
 }
