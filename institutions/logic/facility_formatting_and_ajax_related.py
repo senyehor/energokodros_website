884dc7d4849dcbed19f1edc2320563d18b18b0e5
Yehor Sencha <senyehor@gmail.com>
2023-10-11 14:07:24 +0300

    ref: improved creating choices for facility

--- institutions/logic/facility_formatting_and_ajax_related.py
+++ institutions/logic/facility_formatting_and_ajax_related.py
@@ -1,28 +1,31 @@
 from builtins import tuple
+from typing import TypeAlias
 
-from django.shortcuts import get_object_or_404
+from django.db.models import QuerySet
 from django.utils.safestring import mark_safe
 
 from institutions.models import Facility
-from utils.forms import _reveal_id, SecureModelChoiceField  # noqa
+from utils.forms import SecureModelChoiceField
+
+Choices: TypeAlias = list[tuple[str, str]]
 
 
-def compose_formatted_institution_facilities_choices(
-        institution: Facility) -> list[tuple[str, str]]:
+def compose_formatted_institution_facilities_choices(institution: Facility) -> Choices:
     if not institution.is_root():
         raise ValueError('provided objects is a facility, not institution')
+    return __compose_choices_for_queryset(Facility.objects.get_all_institution_objects(institution))
+
+
+def compose_formatted_facility_descendants_and_itself(facility: Facility) -> Choices:
+    return __compose_choices_for_queryset(facility.get_tree(facility))
+
+
+def __compose_choices_for_queryset(qs: QuerySet) -> Choices:
     choices = SecureModelChoiceFieldWithVerboseFacilityLabeling(
-        queryset=Facility.objects.get_all_institution_objects(institution),
+        queryset=qs,
         empty_label=None
     ).choices
     return [(str(value), label) for value, label in choices]
-
-
-def get_institution_by_hashed_id(hashed_institution_id: str) -> Facility:
-    return get_object_or_404(
-        Facility,
-        pk=_reveal_id(hashed_institution_id)
-    )
 
 
 class SecureModelChoiceFieldWithVerboseFacilityLabeling(SecureModelChoiceField):
