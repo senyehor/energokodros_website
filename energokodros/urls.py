9901101ee6ba60a38a60c58be832bfd622d21db7
Illia Volkov <kuza617@gmail.com>
2023-10-12 14:23:00 +0300

    feat: added energy urls

--- energokodros/urls.py
+++ energokodros/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     re_path(r'^$|home', index_view, name='home'),
     path('users/', include('users.urls')),
     path('administrator/', include('administrator.urls')),
+    path('energy/', include('energy.urls'))
 ]
