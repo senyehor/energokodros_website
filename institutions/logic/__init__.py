f5ca4395365c023676245f0b5fe86d03389336b8
Illia Volkov <kuza617@gmail.com>
2023-10-12 12:25:43 +0300

    feat: changes __init__`s corresponding to changes

--- institutions/logic/__init__.py
+++ institutions/logic/__init__.py
@@ -1,4 +1,4 @@
 from .facility_formatting_and_ajax_related import (
+    compose_formatted_facility_descendants_and_itself,
     compose_formatted_institution_facilities_choices,
-    get_institution_by_hashed_id,
 )
