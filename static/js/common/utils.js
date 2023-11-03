bfc89eac08bf3a82e872486a526d18e5b24fe05e
Illia Volkov <kuza617@gmail.com>
2023-11-03 14:07:23 +0300

    feat: created utils.js to hold reusable general code

--- static/js/common/utils.js
+++ static/js/common/utils.js
@@ -16,3 +16,31 @@ function update_select_with_options(select, options) {
         )
     }
 }
+
+function add_success_alert(message) {
+    __get_alerts_container_create_if_not_exists().append(__create_alert_div('success', message));
+}
+
+function add_error_alert(message) {
+    __get_alerts_container_create_if_not_exists().append(__create_alert_div('danger', message));
+}
+
+function __create_alert_div(level, message) {
+    return '' +
+        `<div class="alert alert-${level} alert-dismissible fade show" role="alert">` +
+        `${message}` +
+        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">' +
+        '</button>' +
+        '</div>'
+}
+
+function __get_alerts_container_create_if_not_exists() {
+    if (!$('#alerts').length) {
+        $('header:first').after('' +
+            '<div class="container">' +
+            '   <div id="alerts">' +
+            '   </div>' +
+            '</div>');
+    }
+    return $("#alerts");
+}
