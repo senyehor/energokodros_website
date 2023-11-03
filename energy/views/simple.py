e47ec1be644dd138e8b5b7de031735e17187d717
Illia Volkov <kuza617@gmail.com>
2023-11-03 13:55:31 +0300

    renaming

--- energy/views/simple.py
+++ energy/views/simple.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, JsonResponse
 
-from energy.logic.ajax_related import get_facilities_formatted_choices_for_user_role
+from energy.logic.ajax import get_facilities_formatted_choices_for_user_role
 from users.logic.simple import check_role_belongs_to_user
 from users.models import UserRole
 from utils.common import get_object_by_hashed_id_or_404
