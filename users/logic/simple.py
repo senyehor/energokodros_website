ea45ea498ecf62dc29275e55da71db61163afc01
Illia Volkov <kuza617@gmail.com>
2023-11-03 14:16:43 +0300

    feat: added check_role_has_access_for_facility

--- users/logic/simple.py
+++ users/logic/simple.py
@@ -3,12 +3,20 @@ from datetime import timedelta
 from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest
 
+from institutions.models import Facility
 from users.models import User, UserRole
 
 
 def check_role_belongs_to_user(user: User, user_role: UserRole):
     if user_role.user != user:
         raise PermissionDenied
+
+
+def check_role_has_access_for_facility(user_role: UserRole, facility: Facility):
+    _ = user_role.facility_has_access_to
+    if not facility.is_descendant_of(_) or facility == _:
+        return
+    raise PermissionDenied
 
 
 def remember_user_for_two_week(request: HttpRequest):
