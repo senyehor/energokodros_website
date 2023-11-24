4d5cea0040e448405d79a8d4a92e7b3f1350e35d
Illia Volkov <kuza617@gmail.com>
2023-11-24 11:32:21 +0300

    ref: better naming

--- administrator/context_processors.py
+++ administrator/context_processors.py
@@ -6,8 +6,8 @@ from administrator.logic import (
 
 def user_roles_applications_to_review_count(request):
     if is_admin(request):
-        _ = get_applications_from_users_who_confirmed_email()
+        count = get_applications_from_users_who_confirmed_email().count()
         return {
-            'users_roles_applications_count': _.count()
+            'users_roles_applications_count': count
         }
     return {}
