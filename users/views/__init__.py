128f381f2c2ea64e5b23fbeefbffa25f992a89f8
Illia Volkov <kuza617@gmail.com>
2023-11-26 10:31:05 +0300

    fix: removed redundant admin rights check

--- users/views/__init__.py
+++ users/views/__init__.py
@@ -1,12 +1,7 @@
 from .create_user_registration_request import CreateUserRegistrationRequestView
 from .simple import (
-    confirm_email,
-    LoginView,
-    successfully_created_registration_request,
-    EditUserView,
-    UserListView,
-    UserRoleApplicationsListView,
+    confirm_email, EditUserRoleView, EditUserView, LoginView, redirect_to_edit_role_by_post_pk,
+    successfully_created_registration_request, UserListView, UserRoleApplicationsListView,
     UserRoleListView,
-    EditUserRoleView
 )
 from .user_role_application_decision import UserRoleApplicationDecisionView
