bf96b3cfaedb506677afe1b770ecdf9653891992
Illia Volkov <kuza617@gmail.com>
2023-12-27 12:43:49 +0300

    feat: add view types

--- utils/types.py
+++ utils/types.py
@@ -1,4 +1,11 @@
-from typing import Any, Tuple, TypeAlias
+from typing import Any, Callable, Tuple, TypeAlias
+
+from django.http import HttpRequest, HttpResponse
+from django.views import View as View_for_typehint
 
 StrTuple: TypeAlias = Tuple[str, ...]
 StrKeyDict: TypeAlias = dict[str, Any]
+FuncView: TypeAlias = Callable[[HttpRequest, ...], HttpResponse]
+ClassView: TypeAlias = View_for_typehint
+ViewType: TypeAlias = FuncView | ClassView
+ViewDecorator: TypeAlias = Callable[[ViewType], ViewType]
