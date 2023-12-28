280bb58caa3ba591558f155e157556832077b4a1
Yehor Sencha <senyehor@gmail.com>
2023-12-28 15:44:44 +0300

    feat: add getting boxes sets for facility

--- energy/logic/aggregated_consumption/simple.py
+++ energy/logic/aggregated_consumption/simple.py
@@ -1,4 +1,5 @@
 import functools
+from typing import Iterable
 
 from django.http import HttpRequest, QueryDict
 from django.shortcuts import render
@@ -6,7 +7,10 @@ from django.shortcuts import render
 from energy.logic.aggregated_consumption.exceptions import QueryParametersInvalid
 from energy.logic.aggregated_consumption.models import AggregationIntervalSeconds
 from energy.logic.aggregated_consumption.parameters import EnergyConsumptionQueryRawParameters
+from energy.models import BoxSensorsSet
+from institutions.models import Facility
 from users.logic import check_user_has_no_roles
+from utils.common.object_to_queryset import object_to_queryset
 from utils.types import FuncView
 
 
@@ -40,3 +44,11 @@ def show_no_roles_page_if_user_has_no_roles(view: FuncView) -> FuncView:
         return view(request, *args, **kwargs)
 
     return wrapper
+
+
+def get_box_set_ids_for_facility(facility: Facility) -> Iterable[int]:
+    if descendants := facility.get_descendants().only('id'):
+        qs = descendants
+    else:
+        qs = object_to_queryset(facility)
+    return BoxSensorsSet.objects.only('pk').filter(facility__in=qs).values_list('pk', flat=True)
