01a6c10868fc2de7c4b7414fbc84b01d92ca0a22
Yehor Sencha <senyehor@gmail.com>
2024-01-20 13:34:19 +0300

    feat: add box list page

--- energy/views/simple.py
+++ energy/views/simple.py
@@ -1,9 +1,17 @@
 from django.http import HttpRequest, JsonResponse
 
 from energy.logic.ajax import get_facilities_formatted_choices_for_user_role
+from energy.models import Box
 from users.logic.simple import check_role_belongs_to_user
 from users.models import UserRole
 from utils.common import get_object_by_hashed_id_or_404
+from utils.views import ListViewWithFiltering
+
+
+class BoxListView(ListViewWithFiltering):
+    queryset = Box.objects.all()
+    filter_fields = ('box_identifier', 'box_description')
+    template_name = 'energy/box_list.html'
 
 
 def get_facilities_choices_for_role(request: HttpRequest) -> JsonResponse:
