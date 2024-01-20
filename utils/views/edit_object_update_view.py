e2212522955a849ab6c715ab7f26d3c7d5715dbe
Yehor Sencha <senyehor@gmail.com>
2024-01-20 13:22:04 +0300

    feat: extracted typehint

--- utils/views/edit_object_update_view.py
+++ utils/views/edit_object_update_view.py
@@ -1,4 +1,4 @@
-from typing import Protocol, runtime_checkable, Union
+from typing import Protocol, runtime_checkable, TypeAlias, Union
 
 from django.contrib import messages
 from django.db.models import Model
@@ -17,6 +17,9 @@ class NeedsAdditionalFilling(Protocol):
         ...
 
 
+EditDeleteObjectView: TypeAlias = Union[UpdateView, 'EditDeleteObjectUpdateViewMixin']
+
+
 class EditDeleteObjectUpdateViewMixin:
     """
     must be used as a mixin for UpdateView, needs to be first in superclass list
@@ -26,29 +29,24 @@ class EditDeleteObjectUpdateViewMixin:
     delete_success_message: str = _('Успішно видалено')
     no_changes_message: str = _('Ви не внесли жодних змін, тож були перенаправлені')
 
-    def post(
-            self: Union[UpdateView, 'EditDeleteObjectUpdateViewMixin'],
-            request: HttpRequest, *args, **kwargs
-    ):
+    def post(self: EditDeleteObjectView, request: HttpRequest, *args, **kwargs):
         if request.POST['submit'] == DEFAULT_DELETE_VALUE:
             return self.__delete_object()
         # noinspection PyUnresolvedReferences
         return super().post(request, *args, **kwargs)
 
-    def get_context_data(self: Union[UpdateView, 'EditDeleteObjectUpdateViewMixin'], **kwargs):
+    def get_context_data(self: EditDeleteObjectView, **kwargs):
         # noinspection PyUnresolvedReferences
         data = super().get_context_data(**kwargs)
         self.fill_from_object_if_needed(data)
         return data
 
-    def fill_from_object_if_needed(
-            self: Union[UpdateView, 'EditDeleteObjectUpdateViewMixin'], data: dict
-    ):
+    def fill_from_object_if_needed(self: EditDeleteObjectView, data: dict):
         form: Form = data['form']
         if isinstance(form, NeedsAdditionalFilling):
             form.additionally_fill(self.object)
 
-    def form_valid(self: Union[UpdateView, 'EditDeleteObjectUpdateViewMixin'], form: Form):
+    def form_valid(self: EditDeleteObjectView, form: Form):
         if form.has_changed():
             messages.success(
                 self.request,
@@ -61,7 +59,7 @@ class EditDeleteObjectUpdateViewMixin:
             )
         return super().form_valid(form)
 
-    def __delete_object(self: Union[UpdateView, 'EditDeleteObjectUpdateViewMixin']):
+    def __delete_object(self: EditDeleteObjectView):
         self.get_object().delete()
         messages.warning(
             self.request,
