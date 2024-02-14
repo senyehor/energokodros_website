63f306e343c3fa9adfd69dbcd6e4ef56550869a2
Yehor Sencha <senyehor@gmail.com>
2024-02-14 14:52:40 +0300

    ref: moved ajax functions to utils and deleted ajax and it`s usages

--- static/js/common/utils.js
+++ static/js/common/utils.js
@@ -93,3 +93,28 @@ const DEFAULT_UNEXPECTED_ERROR_MESSAGE = '' +
     'зверніться до адміністратора.'
 
 const ON_CLICK_REDIRECTS_TEXT = 'При натисканні вас перенаправить на сторінку'
+
+function redirect_to_object(redirect_url, id) {
+    $.ajax({
+        url: reverse_url(redirect_url),
+        type: 'POST',
+        dataType: 'json',
+        headers: get_headers_for_ajax_object(),
+        data: {id: id},
+        success: (data) => {
+            window.open(data.url, '_self');
+        },
+        error: (data) => {
+            add_error_alert(DEFAULT_UNEXPECTED_ERROR_MESSAGE);
+        }
+    });
+}
+
+function redirect_to_selected_object(redirect_url) {
+    // clearing selected as when users go back to page option remains selected
+    return function () {
+        let select = $(this);
+        let id = get_selected_option_for_select(select);
+        redirect_to_object(redirect_url, id);
+    }
+}
