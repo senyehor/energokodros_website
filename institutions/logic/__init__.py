1cf92820efc6ec67069b6fbadb4c3a5a066d604b
Yehor Sencha <senyehor@gmail.com>
2024-02-15 12:20:16 +0300

    feat: add get all institution facilities for a facility

--- institutions/logic/__init__.py
+++ institutions/logic/__init__.py
@@ -2,5 +2,7 @@ from .facility_formatting_and_ajax_related import (
     common_facility_choices_format_function, compose_formatted_facility_descendants_and_itself,
     compose_formatted_institution_facilities_choices,
 )
-
-from .simple import label_from_user_role_for_facility_roles
+from .simple import (
+    get_all_facilities_of_facility_institution,
+    label_from_user_role_for_facility_roles,
+)
