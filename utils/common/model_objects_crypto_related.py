d620a2cec869880b888da1c21ccd84c7a88ae37e
Illia Volkov <kuza617@gmail.com>
2024-02-17 10:40:11 +0300

    fix: change to actually raise 404 instead of returning empty model qs

--- utils/common/model_objects_crypto_related.py
+++ utils/common/model_objects_crypto_related.py
@@ -2,6 +2,7 @@ from typing import Any, Callable
 
 from django.db.models import Manager, Model, QuerySet
 from django.db.models.base import ModelBase
+from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 from utils.forms import INT_HIDER, INT_REVEALER, SecureModelChoiceField
@@ -23,8 +24,8 @@ def compose_secure_choices_for_queryset(
 def get_object_by_hashed_id_or_404(_class: ModelBase | Manager | QuerySet, hashed_id: str) -> Model:
     try:
         return get_object_or_404(_class, pk=INT_REVEALER(hashed_id))
-    except ValueError:  # failed to unhash id
-        return _class.none()
+    except ValueError as e:
+        raise Http404 from e
 
 
 def hash_id(model_object: Model) -> str:
