8f722e8deb63e4655180569b362924d71760576a
Illia Volkov <kuza617@gmail.com>
2024-04-30 11:50:10 +0300

    feat: create run aggregation foundation

---
+++ energy/logic/run_aggregation/bases.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+from energy.logic.run_aggregation.models_and_constants import AggregationStates
+
+
+class AggregationRunner(ABC):
+    @abstractmethod
+    def run_aggregation(self):
+        ...
+
+
+class AggregationStateRetriever(ABC):
+    @abstractmethod
+    def get_state(self) -> AggregationStates:
+        ...
+
+    @abstractmethod
+    def get_last_time_aggregation_was_run(self) -> datetime | None:
+        ...
