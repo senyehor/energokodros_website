f100bad9c7e62199fa83521262488364f7a5854e
Illia Volkov <kuza617@gmail.com>
2024-06-13 11:48:53 +0300

    ref: removed filtering logic from parameters

--- energy/logic/aggregated_consumption/parameters.py
+++ energy/logic/aggregated_consumption/parameters.py
@@ -1,6 +1,5 @@
-import datetime
 from dataclasses import dataclass
-from datetime import date, datetime, time
+from datetime import date
 from enum import Enum
 from typing import TypeAlias, TypedDict, Union
 
@@ -30,31 +29,22 @@ class CommonQueryParameters:
     period_end: date
 
     @property
-    def period_end(self) -> datetime:
-        return datetime.combine(
-            self._period_end,
-            self.__create_all_zeros_time()
-        )
-
-    @period_end.setter
-    def period_end(self, value: datetime):
-        # noinspection PyAttributeOutsideInit
-        self._period_end = value
-
-    @property
-    def period_start(self) -> datetime:
-        return datetime.combine(
-            self._period_start,
-            self.__create_all_zeros_time()
-        )
+    def period_start(self) -> date:
+        return self._period_start
 
     @period_start.setter
     def period_start(self, value: date):
         # noinspection PyAttributeOutsideInit
         self._period_start = value
 
-    def __create_all_zeros_time(self) -> time:
-        return time(0, 0, 0)
+    @property
+    def period_end(self) -> date:
+        return self._period_end
+
+    @period_end.setter
+    def period_end(self, value: date):
+        # noinspection PyAttributeOutsideInit
+        self._period_end = value
 
 
 @dataclass(kw_only=True)
@@ -75,37 +65,9 @@ class OneHourAggregationIntervalQueryParameters(CommonQueryParameters):
             and self.__hour_filtering_method_every_day
 
     @property
-    def period_start(self) -> datetime:
-        if self.__hour_filtering_method_whole_interval:
-            return datetime.combine(
-                self._period_start,
-                time(self.hours_filtering_start_hour, 00, 00)
-            )
-        return super().period_start
-
-    @property
-    def period_end(self) -> datetime:
-        if self.__hour_filtering_method_whole_interval:
-            return datetime.combine(
-                self._period_end,
-                time(self.hours_filtering_end_hour, 00, 00)
-            )
-        return super().period_end
-
-    @property
     def __hour_filtering_method_every_day(self) -> bool:
         return self.hours_filtering_method == self.HourFilteringMethods.EVERY_DAY
 
     @property
     def __hour_filtering_method_whole_interval(self) -> bool:
         return self.hours_filtering_method == self.HourFilteringMethods.WHOLE_INTERVAL
-
-    @period_start.setter
-    def period_start(self, value: date):
-        # noinspection PyAttributeOutsideInit
-        self._period_start = value
-
-    @period_end.setter
-    def period_end(self, value: datetime):
-        # noinspection PyAttributeOutsideInit
-        self._period_end = value
