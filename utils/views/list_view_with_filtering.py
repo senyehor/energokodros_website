6100935b6f6d421aa44a99ce876630f0721a6a90
Illia Volkov <kuza617@gmail.com>
2024-07-12 13:59:14 +0300

    ref: move filter class belong to a mixin using aggregation, instead of being used straightaway in a method

--- utils/views/list_view_with_filtering.py
+++ utils/views/list_view_with_filtering.py
@@ -10,33 +10,6 @@ _ListViewWithMixinType: TypeAlias = Union[ListView, 'QuerySetFieldsIcontainsFilt
 DEFAULT_PAGINATE_BY = 7
 
 
-class QuerySetFieldsIcontainsFilterPkOrderedMixin:
-    """this mixin is supposed to be used with ListViews"""
-    filter_fields: StrTuple = None
-    fields_order_by_before_pk: StrTuple = tuple()
-
-    def get_queryset(self: _ListViewWithMixinType) -> QuerySet:
-        self.__check_used_properly()
-        if search_value := self.__get_search_value():
-            qs = QuerySetFieldsIcontainsFilter(
-                self.queryset,
-                self.filter_fields,
-            ).filter(search_value)
-        else:
-            qs = self.queryset
-        return qs.order_by(*self.fields_order_by_before_pk, '-pk')
-
-    def __check_used_properly(self: _ListViewWithMixinType):
-        if not issubclass(self.__class__, ListView):
-            raise ValueError('this mixin must be used with a ListView')
-        if self.filter_fields is None:
-            raise ValueError('you must set filter_fields for a model')
-        return True
-
-    def __get_search_value(self: ListView) -> str:
-        return self.request.GET.get('search_value', None)
-
-
 class QuerySetFieldsIcontainsFilter:
     def __init__(self, qs: QuerySet, fields_to_filter: Iterable[str]):
         self._qs = qs
@@ -49,6 +22,37 @@ class QuerySetFieldsIcontainsFilter:
             Q()
         )
         return self._qs.filter(_filter)
+
+
+class QuerySetFieldsIcontainsFilterPkOrderedMixin:
+    """this mixin is supposed to be used with ListViews"""
+    filter_fields: StrTuple = None
+    fields_order_by_before_pk: StrTuple = tuple()
+    __filter = QuerySetFieldsIcontainsFilter
+
+    def get_queryset(self: _ListViewWithMixinType) -> QuerySet:
+        self.__check_used_properly()
+        if search_value := self.__get_search_value():
+            qs = self.__filter_queryset_for_value(search_value)
+        else:
+            qs = self.queryset
+        return qs.order_by(*self.fields_order_by_before_pk, '-pk')
+
+    def __filter_queryset_for_value(self: _ListViewWithMixinType, value: str) -> QuerySet:
+        return self.__filter(
+            self.queryset,
+            self.filter_fields,
+        ).filter(value)
+
+    def __check_used_properly(self: _ListViewWithMixinType):
+        if not issubclass(self.__class__, ListView):
+            raise ValueError('this mixin must be used with a ListView')
+        if self.filter_fields is None:
+            raise ValueError('you must set filter_fields for a model')
+        return True
+
+    def __get_search_value(self: ListView) -> str:
+        return self.request.GET.get('search_value', None)
 
 
 class ListViewWithFiltering(QuerySetFieldsIcontainsFilterPkOrderedMixin, ListView):
