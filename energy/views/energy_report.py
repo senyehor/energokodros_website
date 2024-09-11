becdfe1af0ecabe80ef9e60e652a2f2641be376f
Illia Volkov <kuza617@gmail.com>
2024-09-11 12:15:01 +0300

    ref: changes due to dependencies changes

--- energy/views/energy_report.py
+++ energy/views/energy_report.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import FileResponse, HttpRequest, JsonResponse
 
 from energy.logic.aggregated_consumption import (
-    AggregatedEnergyWithOptionalForecastQuerier,
+    ConsumptionWithOptionalForecastQuerierFromRawParameter,
     convert_request_post_dict_to_regular_dict, EnergyConsumptionExceptionWithMessage,
 )
 from energy.logic.consumption_report.docx import ReportCreator
@@ -14,12 +14,20 @@ def get_energy_report(request: HttpRequest):
     # noinspection PyTypeChecker
     raw_parameters = convert_request_post_dict_to_regular_dict(request.POST)
     try:
-        controller = AggregatedEnergyWithOptionalForecastQuerier(request.user, raw_parameters)
-        consumption_with_optional_forecast, total_consumption = \
-            controller.get_consumption_and_total_consumption_with_optional_forecast()
+        querier = ConsumptionWithOptionalForecastQuerierFromRawParameter(
+            request.user, raw_parameters
+        )
+        raw_and_formatted_consumption_with_raw_and_formatted_optional_forecast = \
+            querier.get_raw_and_formatted_consumption_with_raw_and_formatted_optional_forecast()
+        total_consumption = querier.get_total_consumption()
     except EnergyConsumptionExceptionWithMessage as e:
         return JsonResponse(e.message, status=400, safe=False)
+    if not raw_and_formatted_consumption_with_raw_and_formatted_optional_forecast:
+        # todo fix via uniform response with metadata
+        return FileResponse(None)
     report = ReportCreator(
-        consumption_with_optional_forecast, total_consumption, controller.get_parameters()
+        raw_and_formatted_consumption_with_raw_and_formatted_optional_forecast,
+        total_consumption,
+        querier.get_parameters()
     ).create_report()
     return FileResponse(report, filename='report.docx')
