ed36458f7cc6dd48fa34db3e5f47d41f6d34cce1
Yehor Sencha <senyehor@gmail.com>
2024-09-14 12:38:43 +0300

    feat: make dateinputs ids contain week as well, as week period will be filtered using them

--- static/js/energy/consumption/helping/elements.js
+++ static/js/energy/consumption/helping/elements.js
@@ -2,8 +2,8 @@ function __get_interval_filtering_div() {
     return $('#interval_filtering_div_id');
 }
 
-function get_hour_or_day_period_input(start_or_end) {
-    return $(`#hour_or_day_period_${start_or_end}_id`)
+function get_hour_or_day_or_week_period_input(start_or_end) {
+    return $(`#hour_or_day_or_week_period_${start_or_end}_id`)
 }
 
 function get_hour_filters_select(start_or_end) {
