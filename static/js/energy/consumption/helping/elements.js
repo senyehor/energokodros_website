f1e75f4da99a4d0a40bc1a3865eaf48f2c57fd31
Illia Volkov <kuza617@gmail.com>
2024-09-14 10:47:04 +0300

    ref: move elements

--- static/js/energy/consumption/helping/elements.js
+++ static/js/energy/consumption/helping/elements.js
@@ -1,3 +1,11 @@
 function __get_interval_filtering_div() {
     return $('#interval_filtering_div_id');
 }
+
+function __get_period_start_input() {
+    return $('#period_start')
+}
+
+function __get_period_end_input() {
+    return $('#period_end')
+}
